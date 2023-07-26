5e6516c55517a138529b3bbe2cf9e607b4206700
Spenser Schwartz <spensernet@gmail.com>
2023-07-26 14:36:09 -0700

    filterUserForClient

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,17 +1,8 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-// import { filterUserForClient } from "~/server/helpers/filterUserForClient";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
