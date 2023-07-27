8c62c7323b7f199b231c1d570ad945d6550580c4
Spenser Schwartz <spensernet@gmail.com>
2023-07-27 13:20:31 -0700

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,21 @@ import { PostView } from "~/components/postView";
 import { PageLayout } from "~/components/layout";
 import { api } from "~/utils/api";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -32,7 +44,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,11 @@
 import { clerkClient } from "@clerk/nextjs";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const postsRouter = createTRPCRouter({
@@ -28,4 +32,18 @@ export const postsRouter = createTRPCRouter({
       return { post, author };
     });
   }),
+
+  create: privateProcedure
+    .input(z.object({ content: z.string().emoji().min(1).max(280) }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+      return post;
+    }),
 });
