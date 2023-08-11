2108159e021967f30af16cf9b4b51438a4964f7f
Spenser Schwartz <spensernet@gmail.com>
2023-08-11 14:01:55 -0700

    Reset to prev commit, remove files

--- src/styles/pages/home/styles.ts
+++
@@ -1,7 +0,0 @@
-import tw from "twin.macro";
-
-export const HomePageContainer = tw.div`h-screen bg-purple-950`;
-
-export const CenterSectionContainer = tw.div`relative isolate overflow-hidden`;
-
-export const GradientContainer = tw.div``;
