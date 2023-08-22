631cedcee28ca8ee9b7482580f99a40fdd7dc3f3
Spenser Schwartz <spensernet@gmail.com>
2023-08-22 16:25:15 -0500

    Add imageURL to Attraction model

--- src/components/imageGrid/index.tsx
+++ src/components/imageGrid/index.tsx
@@ -19,7 +19,7 @@ const files = [
     title: "IMG_4986.HEIC",
     size: "3.9 MB",
     source:
-      "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
+      "https://images.unsplash.com/photo-1529180184525-78f99adb8e98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
   },
   {
     title: "IMG_4987.HEIC",
@@ -50,6 +50,7 @@ interface ImageGridProps {
 
 const ImageGrid = ({ cityData }: ImageGridProps) => {
   console.log("ImageGrid cityData", cityData);
+  console.log("attractions", cityData?.attractions);
   return (
     <ul role="list" className={imageGridClassName}>
       {files.map((file) => (
