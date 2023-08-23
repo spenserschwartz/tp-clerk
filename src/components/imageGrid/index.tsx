696437635453adf9360966c5128f792d2c2ac3f0
Spenser Schwartz <spensernet@gmail.com>
2023-08-22 20:49:09 -0500

    City Page in progress

--- src/components/imageGrid/index.tsx
+++ src/components/imageGrid/index.tsx
@@ -7,74 +7,35 @@ import {
 } from "./styles";
 
 import type { RouterOutputs } from "~/utils/api";
-
-const files = [
-  {
-    title: "IMG_4985.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
-  },
-  {
-    title: "IMG_4986.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1529180184525-78f99adb8e98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-  },
-  {
-    title: "IMG_4987.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
-  },
-  {
-    title: "IMG_4988.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
-  },
-  {
-    title: "IMG_4989.HEIC",
-    size: "3.9 MB",
-    source:
-      "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
-  },
-  // More files...
-];
-
 type GetCityByNameType = RouterOutputs["city"]["getCityByName"];
-
 interface ImageGridProps {
   cityData: GetCityByNameType;
 }
 
 const ImageGrid = ({ cityData }: ImageGridProps) => {
   console.log("ImageGrid cityData", cityData);
-  console.log("attractions", cityData?.attractions);
+
+  const { attractions } = cityData ?? { attractions: null };
+  console.log("attractions", attractions);
+
   return (
     <ul role="list" className={imageGridClassName}>
-      {files.map((file) => (
-        <li key={file.title} className={listItemClassName}>
+      {attractions?.map((attraction) => (
+        <li key={attraction.id} className={listItemClassName}>
           <div className={imageContainerClassName}>
             <Image
-              src={file.source}
+              src={attraction?.imageURL as string}
               alt=""
               className={imageClassName}
               width={100}
               height={100}
             />
-            {/* <button
-              type="button"
-              className="absolute inset-0 focus:outline-none"
-            >
-              <span className="sr-only">View details for {file.title}</span>
-            </button> */}
           </div>
           <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-            {file.title}
+            {attraction.name}
           </p>
           <p className="pointer-events-none block text-sm font-medium text-gray-500">
-            {file.size}
+            {attraction.name}
           </p>
         </li>
       ))}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="overflow-none flex h-screen justify-center bg-purple-500">
+    <main className="overflow-none flex h-screen justify-center bg-purple-800">
       <div className="flex h-full w-full max-w-6xl flex-col border-x border-slate-400 ">
         {props.children}
       </div>
