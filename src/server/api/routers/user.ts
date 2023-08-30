bf8fbdc46dadc5b89adc4005dcca23272ad1f811
Spenser Schwartz <spensernet@gmail.com>
2023-08-30 16:49:39 -0700

    userRouter create in progress

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,19 @@
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+// Create a new ratelimiter, that allows 1 request per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(1, "1 m"),
+  analytics: true,
+});
 
 export const userRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -10,6 +23,25 @@ export const userRouter = createTRPCRouter({
     });
     return users;
   }),
+
+  create: privateProcedure
+    .input(z.object({ email: z.string(), name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+      const newUser = await ctx.prisma.user.create({
+        data: {
+          id: authorId,
+          email: input.email,
+          name: input.name,
+        },
+      });
+
+      return newUser;
+    }),
 
   // More routers here...
 });
