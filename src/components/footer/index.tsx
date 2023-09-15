d881088a72f17ef927f95e620696fd6c78593afc
Spenser Schwartz <spensernet@gmail.com>
2023-09-15 15:20:36 -0700

    Update margin

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
               </a>
             ))}
           </div>
-          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+          <p className="mb-2 mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
             &copy; 2023 Spenser Schwartz, All rights reserved.
           </p>
         </div>
