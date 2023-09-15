bc47da45defd85ffba8425a78f63fd7015bf0acf
Spenser Schwartz <spensernet@gmail.com>
2023-09-15 11:50:37 -0700

    placeholder image

--- src/components/imageGrid/components/gridElement.tsx
+++ src/components/imageGrid/components/gridElement.tsx
@@ -75,7 +75,7 @@ const GridElement = ({
     >
       <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
         <Image
-          src={attraction.imageURL}
+          src={attraction.imageURL || "/images/placeholder.png"}
           alt=""
           className="pointer-events-none object-cover group-hover:opacity-75"
           width={100}
