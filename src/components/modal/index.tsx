77fc9d87bbb9a1c4645448665e907be749ea025e
Spenser Schwartz <spensernet@gmail.com>
2023-09-19 18:14:46 -0700

    Modal in progress

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -40,7 +40,7 @@ export default function Modal({ openModal, setOpenModal }: ModalProps) {
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
                     <CheckIcon
                       className="h-6 w-6 text-green-600"
                       aria-hidden="true"
@@ -62,6 +62,31 @@ export default function Modal({ openModal, setOpenModal }: ModalProps) {
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-6">
+                  <div className="mt-2">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="you@example.com"
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={closeModalHandler}
+                  >
+                    Go back to dashboard
+                  </button> */}
+                  <div className="mt-2">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="you@example.com"
+                    />
+                  </div>
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
