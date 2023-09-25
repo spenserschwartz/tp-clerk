b3be83cb462a8d18192f83c95a6d68fa23e7be9b
Spenser Schwartz <spensernet@gmail.com>
2023-09-25 10:25:33 -0700

    Mobile header to TravelPerfect

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -75,13 +75,14 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">TravelPerfect</span>
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
+              {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                 alt=""
-              />
+              /> */}
+              <p className="text-lg font-bold text-purple-500">TravelPerfect</p>
             </a>
             <button
               type="button"
