69e1b7f048e31473ed008303663bafb4261e7b85
Spenser Schwartz <spensernet@gmail.com>
2023-09-25 12:36:41 -0700

    Cleanup

--- src/pages/cities/index.tsx
+++ src/pages/cities/index.tsx
@@ -1,28 +1,35 @@
 import { type NextPage } from "next";
 import Link from "next/link";
 import React from "react";
+import { LoadingPage } from "~/components";
 import { api } from "~/utils/api";
 
 const CitiesPage: NextPage = () => {
-  const { data: cityNames } = api.city.getAllCityNames.useQuery();
+  const { data: cityNames, isLoading } = api.city.getAllCityNames.useQuery();
 
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <p className="text-center">
-        These are the current cities that TravelPerfect is working with:
-      </p>
-      <p className="text-orange-400">(Click to navigate to city)</p>
-      <ul className="flex flex-col  text-center">
-        {cityNames?.map((cityName) => (
-          <Link
-            className="mt-2"
-            key={`CitiesList:${cityName}`}
-            href={`/city/${cityName}`}
-          >
-            {cityName}
-          </Link>
-        ))}
-      </ul>
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <>
+          <p className="text-center">
+            These are the current cities that TravelPerfect is working with:
+          </p>
+          <p className="text-orange-400">(Click to navigate to city)</p>
+          <ul className="flex flex-col  text-center">
+            {cityNames?.map((cityName) => (
+              <Link
+                className="mt-2"
+                key={`CitiesList:${cityName}`}
+                href={`/city/${cityName}`}
+              >
+                {cityName}
+              </Link>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
