0591d98789d6eeaea599fed82b9f4a9334ef2db4
Spenser Schwartz <spensernet@gmail.com>
2023-09-27 10:28:21 -0700

    openai working on splash page, cleanup needed

--- src/pages/api/openai.ts
+++ src/pages/api/openai.ts
@@ -1,12 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import openai from "~/utils/openai";
 
 interface Data {
   name: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content:
+          "Can you generate a response to list the top 3 attractions in London? The response should be in JSON format and include the name, description, and location of each attraction.",
+      },
+    ],
+  });
+
+  const responseText =
+    chatCompletion.choices[0]?.message.content ?? "No AI response found";
+  //   res.status(200).json({ responseText });
+
+  const responseObject = JSON.parse(responseText);
+  res.status(200).json(responseObject);
+
+  // Receive some text, ask ChatGPT to generate something
 }
--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,5 +1,30 @@
+import { MouseEvent } from "react";
+
 const SplashPage = () => {
-  return <div>SplashPage</div>;
+  const handleGenerateTextWithAI = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    console.log("handleGenerateTextWithAI");
+
+    try {
+      console.log("trying to hit..");
+      const res = await fetch(`/api/openai`);
+      if (res.status !== 200) console.error("Error generating text with AI");
+      else {
+        console.log("hit");
+        const data = await res.json();
+        console.log("data", data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleGenerateTextWithAI}>Click for AI</button>
+    </div>
+  );
 };
 
 export default SplashPage;
