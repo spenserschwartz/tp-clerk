d021b8af632beaf76e7c2b66910e8b757f70c0ef
Spenser Schwartz <spensernet@gmail.com>
2023-09-29 11:50:02 -0700

    Remove unused imports

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,9 +1,3 @@
-import { type MouseEvent, useState } from "react";
-import toast from "react-hot-toast";
-import { LoadingSpinner } from "~/components";
-import QuickLaunchForm from "~/components/forms/quickLaunch";
-import { api } from "~/utils/api";
-
 const SplashPage = () => {
   return <div>Splash Page</div>;
 };
