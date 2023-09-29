f8aef0812ed80ba244b15566d4e0d6d9c123df1a
Spenser Schwartz <spensernet@gmail.com>
2023-09-29 09:55:53 -0700

    Update

--- src/pages/quick-launch/index.tsx
+++ src/pages/quick-launch/index.tsx
@@ -1,14 +1,16 @@
 import { type NextPage } from "next";
 import { type MouseEvent, useState } from "react";
 import toast from "react-hot-toast";
-import { LoadingSpinner } from "~/components";
+import { LoadingSpinner, Modal } from "~/components";
 import { api } from "~/utils/api";
 
 const QuickLaunchPage: NextPage = () => {
   const [aiText, setAiText] = useState("aiText will be generated here");
-
   const { mutate, isLoading: isLoadingAI } =
     api.openAI.generateTripItinerary.useMutation({});
+
+  const { data: cityNames, isLoading: cityNamesLoading } =
+    api.city.getAllCityNames.useQuery();
 
   const handleGenerateTextWithAI = () => {
     console.log("handleGenerateTextWithAI");
@@ -16,6 +18,12 @@ const QuickLaunchPage: NextPage = () => {
       onSettled(data, error, variables, context) {
         if (error) console.error(error);
         if (data) console.log("data", data);
+
+        const parsedData = JSON.parse(
+          data?.choices[0]?.message.content ?? "{}"
+        ) as Record<string, string>;
+
+        console.log("PARSE", parsedData);
 
         setAiText(data?.choices[0]?.message.content ?? "");
       },
@@ -28,7 +36,7 @@ const QuickLaunchPage: NextPage = () => {
         <LoadingSpinner size={64} />
       ) : (
         <div>
-          <button onClick={handleGenerateTextWithAI}>Click for AI</button>
+          <button onClick={handleGenerateTextWithAI}>Generate Your Trip</button>
           <p>{aiText}</p>
         </div>
       )}
