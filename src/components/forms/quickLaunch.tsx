b39a74e4c775eb1901c60d1c72cef86c28eb418e
Spenser Schwartz <spensernet@gmail.com>
2023-09-29 10:56:23 -0700

    Update

--- src/components/forms/quickLaunch.tsx
+++ src/components/forms/quickLaunch.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface QuickLaunchFormProps {
   cityNames: string[] | undefined;
 }
 
 const QuickLaunchForm = ({ cityNames }: QuickLaunchFormProps) => {
+  const [numberOfDays, setNumberOfDays] = useState("");
   if (!cityNames?.length) return <div>No city names found</div>;
+
+  const handleNumberOfDaysChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    if (isNaN(Number(e.target.value))) return;
+    else setNumberOfDays(e.target.value);
+  };
 
   return (
     <div className="mx-auto w-full md:w-96 md:max-w-full">
@@ -27,17 +35,7 @@ const QuickLaunchForm = ({ cityNames }: QuickLaunchFormProps) => {
           </label>
 
           <label className="mb-6 block">
-            <span className="text-gray-300">Your name</span>
-            <input
-              name="name"
-              type="text"
-              className="mt-1 w-full rounded-md border-gray-600 bg-transparent text-gray-300 placeholder-gray-600 shadow-sm selection:block focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="Joe Bloggs"
-            />
-          </label>
-
-          <label className="mb-6 block">
-            <span className="text-gray-300">When is your birthday?</span>
+            <span className="text-gray-300">When do you want to start?</span>
             <input
               name="birthday"
               type="date"
@@ -45,33 +43,32 @@ const QuickLaunchForm = ({ cityNames }: QuickLaunchFormProps) => {
             />
           </label>
 
-          <div className="mb-6">
-            <div className="mt-2">
-              <div>
-                <label className="inline-flex items-center">
-                  <input
-                    name="season"
-                    type="radio"
-                    className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                  />
-                  <span className="ml-2 text-gray-300">I like summer</span>
-                </label>
-              </div>
-              <div>
-                <label className="inline-flex items-center">
-                  <input
-                    name="season"
-                    type="radio"
-                    className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                  />
-                  <span className="ml-2 text-gray-300">
-                    I am more into winter
-                  </span>
-                </label>
-              </div>
-            </div>
+          <label className="mb-6 block">
+            <span className="text-gray-300">How long will you be there?</span>
+            <input
+              name="name"
+              type="text"
+              className="mt-1 w-full rounded-md border-gray-600 bg-transparent text-gray-300 placeholder-gray-600 shadow-sm selection:block focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              placeholder="# of days"
+              value={numberOfDays}
+              onChange={handleNumberOfDaysChange}
+            />
+          </label>
+
+          <div>
+            <label className="">
+              <input
+                name="season"
+                type="checkbox"
+                className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
+              />
+              <span className="ml-2 text-gray-300">
+                Add some extra adventure!
+              </span>
+            </label>
           </div>
-          <div className="mb-6">
+
+          <div className="mt-6 flex justify-center">
             <button
               type="submit"
               className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
