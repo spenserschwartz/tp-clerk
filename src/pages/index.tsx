0af8aeea8fca810a4d4c3c080ef0c3565d768f98
Spenser Schwartz <spensernet@gmail.com>
2023-10-04 16:36:28 -0700

    shadcn initialize

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
+import React from "react";
 import { type ReactElement } from "react";
 
 import Combobox from "src/components/combobox";
 import { LoadingPage } from "src/components/loading";
 import { PageLayout } from "~/components";
+import { Calendar } from "~/ui/calendar";
+import { DatePickerWithRange } from "~/ui/datePickerWithRange";
+
 import { api } from "~/utils/api";
 
 const Home = () => {
   const { data, isLoading } = api.city.getAll.useQuery();
+
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   if (isLoading) return <LoadingPage />;
 
@@ -15,51 +21,60 @@ const Home = () => {
   }) ?? [{ name: "no cities found", id: "nocitiesfound" }];
 
   return (
-    <main>
-      {/* Hero section */}
-      <div className="relative isolate overflow-hidden">
-        {/* Gradient */}
-        <div
-          className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#000000] to-[#4f46e5] opacity-20"
-            style={{
-              clipPath:
-                "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
-            }}
-          />
-        </div>
+    // <main>
+    //   {/* Hero section */}
+    //   <div className="relative isolate overflow-hidden">
+    //     {/* Gradient */}
+    //     <div
+    //       className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+    //       aria-hidden="true"
+    //     >
+    //       <div
+    //         className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#000000] to-[#4f46e5] opacity-20"
+    //         style={{
+    //           clipPath:
+    //             "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+    //         }}
+    //       />
+    //     </div>
 
-        {/* Main stuff */}
-        <div className="mx-auto flex max-w-7xl justify-center  px-6 pb-20 pt-10">
-          {/* Center most of everything */}
-          <div className="mx-auto flex max-w-2xl flex-shrink-0 flex-col items-center  px-2 lg:mx-0 lg:max-w-xl lg:pt-8">
-            {/* Title */}
-            <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-8xl">
-              TravelPerfect
-            </h1>
+    //     {/* Main stuff */}
+    //     <div className="mx-auto flex max-w-7xl justify-center  px-6 pb-20 pt-10">
+    //       {/* Center most of everything */}
+    //       <div className="mx-auto flex max-w-2xl flex-shrink-0 flex-col items-center  px-2 lg:mx-0 lg:max-w-xl lg:pt-8">
+    //         {/* Title */}
+    //         <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-8xl">
+    //           TravelPerfect
+    //         </h1>
 
-            {/* Subtitle */}
-            <h2 className="my-5 text-center text-4xl font-bold tracking-tight text-white sm:text-4xl">
-              Where do you want to go?
-            </h2>
+    //         {/* Subtitle */}
+    //         <h2 className="my-5 text-center text-4xl font-bold tracking-tight text-white sm:text-4xl">
+    //           Where do you want to go?
+    //         </h2>
 
-            {/* Container for input and DatePicker */}
-            <div className="mt-2 w-80">
-              <Combobox options={comboboxOptions} />
-            </div>
+    //         {/* Container for input and DatePicker */}
+    //         <div className="mt-2 w-80">
+    //           <Combobox options={comboboxOptions} />
+    //         </div>
 
-            {/* Flavor Text */}
-            <p className="mt-6 text-justify text-lg leading-8 text-gray-300">
-              TravelPerfect is a travel planning tool that helps you find the
-              perfect destination for your next vacation. Enter a city and your
-              desired travel dates to get started.
-            </p>
-          </div>
-        </div>
-      </div>
+    //         {/* Flavor Text */}
+    //         <p className="mt-6 text-justify text-lg leading-8 text-gray-300">
+    //           TravelPerfect is a travel planning tool that helps you find the
+    //           perfect destination for your next vacation. Enter a city and your
+    //           desired travel dates to get started.
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
+    <main className="">
+      {/* <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+      /> */}
+      <DatePickerWithRange />
     </main>
   );
 };
