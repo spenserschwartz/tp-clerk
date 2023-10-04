d8f569eec7b13decfd45302e6eb3a29541af8a8d
Spenser Schwartz <spensernet@gmail.com>
2023-10-04 13:21:10 -0700

    tailwindconfig from juliusmarminge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,36 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js", // datepicker
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"), // Images with details from https://tailwindui.com/components/application-ui/lists/grid-lists
     require("@tailwindcss/forms"), // Combobox
+    require("tailwindcss-animate"),
   ],
 } satisfies Config;
