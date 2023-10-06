244c5e04ef406e3110298289a750f5a879a43dce
Spenser Schwartz <spensernet@gmail.com>
2023-10-06 13:20:06 -0700

    Bring in footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import { type ReactElement } from "react";
 
 import { LoadingPage } from "src/components/loading";
+import Features from "~/components/features";
 import Hero from "~/components/hero";
 import { RootLayout } from "~/components/layout";
+import Footer from "~/components/ui/footer";
 
 import { api } from "~/utils/api";
 
@@ -18,9 +19,8 @@ const Home = () => {
   return (
     <>
       <Hero />
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
+      <Features />
+      <Footer />
     </>
   );
 };
