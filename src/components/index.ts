c7c3bdaacc1fbf8eea5adbe19d52ca71b5c10729
Spenser Schwartz <spensernet@gmail.com>
2023-10-06 09:24:35 -0700

    Introduce RootLayout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import Button from "./button";
 import Footer from "./footer";
 import ImageGrid from "./imageGrid";
-import { PageLayout } from "./layout";
 import { LoadingPage, LoadingSpinner } from "./loading";
 import Modal from "./modal";
 import Searchbar from "./searchbar";
@@ -13,6 +12,5 @@ export {
   LoadingPage,
   LoadingSpinner,
   Modal,
-  PageLayout,
   Searchbar,
 };
