329cd32a73108b26c9a74d3ddb8f6f51b8e84e60
Spenser Schwartz <spensernet@gmail.com>
2023-10-06 14:14:24 -0700

    Update footer

---
+++ src/components/ui/utils.tsx
@@ -0,0 +1,17 @@
+import GitHubIcon from "~/icons/GitHub";
+import LinkedInIcon from "~/icons/LinkedIn";
+
+export const footerNavigation = {
+  socialMediaIcons: [
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/spenser-schwartz/",
+      icon: LinkedInIcon,
+    },
+    {
+      name: "GitHub",
+      href: "https://www.github.com/spenserschwartz",
+      icon: GitHubIcon,
+    },
+  ],
+};
