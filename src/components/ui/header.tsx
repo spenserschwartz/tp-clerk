4938e69249021f2052f81e904d6f8aa26e7152e8
Spenser Schwartz <spensernet@gmail.com>
2023-10-09 15:30:33 -0700

    Update SignIn, SignUp, and SignOut buttons in header via Clerk

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 
+import { SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { headerNavigation } from "./utils";
 // import Dropdown from '@/components/utils/dropdown'
@@ -58,33 +59,42 @@ export default function Header() {
               </div>
 
               {/* Header Sign In Links */}
-              <li>
-                <Link
-                  href="/sign-in"
-                  className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
-                >
-                  Sign in
-                </Link>
-              </li>
-              {/* Header Sign Up */}
-              <li>
-                <Link
-                  href="/sign-in"
-                  className="btn-sm ml-3 bg-gray-900 text-gray-200 hover:bg-gray-800"
-                >
-                  <span>Sign up</span>
-                  <svg
-                    className="-mr-1 ml-2 h-3 w-3 shrink-0 fill-current text-gray-400"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
+              <SignedIn>
+                <SignOutButton>
+                  <button className="btn-sm ml-3 bg-gray-900 text-gray-200 hover:bg-gray-800">
+                    Sign Out
+                  </button>
+                </SignOutButton>
+              </SignedIn>
+              <SignedOut>
+                <li>
+                  <Link
+                    href="/sign-in"
+                    className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
                   >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </Link>
-              </li>
+                    Sign in
+                  </Link>
+                </li>
+                {/* Header Sign Up */}
+                <li>
+                  <Link
+                    href="/sign-in"
+                    className="btn-sm ml-3 bg-gray-900 text-gray-200 hover:bg-gray-800"
+                  >
+                    <span>Sign up</span>
+                    <svg
+                      className="-mr-1 ml-2 h-3 w-3 shrink-0 fill-current text-gray-400"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                        fillRule="nonzero"
+                      />
+                    </svg>
+                  </Link>
+                </li>
+              </SignedOut>
             </ul>
           </nav>
 
