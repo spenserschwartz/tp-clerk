8014a0ce0d138a82f142fc2f3b08ff4294ce68de
Spenser Schwartz <spensernet@gmail.com>
2023-10-10 09:56:47 -0700

    S3 images being displayed

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,5 +1,3 @@
-import Carousel from "~/components/carousel";
-
 const slides = [
   "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
   "https://images.unsplash.com/photo-1529180184525-78f99adb8e98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
@@ -10,9 +8,9 @@ const slides = [
 const SplashPage = () => {
   return (
     <div>
-      <div className="w-64">
-        <Carousel slides={slides} />
-      </div>
+      hey
+      <div className="w-64">Splash Page</div>
+      <img src="https://travelperfect-bucket.s3.us-west-1.amazonaws.com/Penguin.png" />
     </div>
   );
 };
