794e70f7d06a3f1e6fa010abbcc6ab1fd520a8f1
Spenser Schwartz <spensernet@gmail.com>
2023-10-11 15:18:56 -0700

    Update FeaturesQuickLaunch import spelling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { type ReactElement } from "react";
 
 import { LoadingPage } from "src/components/loading";
 import Features from "~/components/features";
-import FeaturesQuickLaunch from "~/components/features-quicklaunch";
+import FeaturesQuickLaunch from "~/components/features-quickLaunch";
 import Hero from "~/components/hero";
 import { RootLayout } from "~/components/layout";
 import QuickLaunch from "~/components/quickLaunch";
