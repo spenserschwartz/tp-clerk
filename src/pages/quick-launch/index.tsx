4d3c7ce22fbdc9cbeab4cb76552a0e85e9745e9c
Spenser Schwartz <spensernet@gmail.com>
2023-10-16 13:17:41 -0700

    Update QuickPage with layout

--- src/pages/quick-launch/index.tsx
+++ src/pages/quick-launch/index.tsx
@@ -1,13 +1,19 @@
-import { type NextPage } from "next";
+import { type ReactElement } from "react";
+import { RootLayout } from "~/components/layout";
 
 import QuickLaunch from "~/components/quickLaunch";
+import { type NextPageWithLayout } from "../_app";
 
-const QuickLaunchPage: NextPage = () => {
+const QuickLaunchPage: NextPageWithLayout = () => {
   return (
-    <div>
+    <div className="mt-16">
       <QuickLaunch />
     </div>
   );
+};
+
+QuickLaunchPage.getLayout = function getLayout(page: ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
 };
 
 export default QuickLaunchPage;
