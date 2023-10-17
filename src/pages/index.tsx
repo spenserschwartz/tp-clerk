92bb1b06cbd96b53f5dcf1a21a2c5ca4f0f6b3a6
Spenser Schwartz <spensernet@gmail.com>
2023-10-17 09:00:53 -0700

    Barrel importing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import { type ReactElement } from "react";
 
-import Features from "~/components/features";
-import FeaturesQuickLaunch from "~/components/features-quickLaunch";
-import Hero from "~/components/hero";
-import { RootLayout } from "~/components/layout";
+import { Features, FeaturesQuickLaunch, Hero, RootLayout } from "~/components";
 
 const Home = () => {
   return (
