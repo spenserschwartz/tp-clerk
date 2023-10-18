aaac419b5ffe73b61502b89f2a5c4902dc7b5071
Spenser Schwartz <spensernet@gmail.com>
2023-10-18 14:10:55 -0700

    gridElement configurations

--- src/components/imageGrid/components/gridElement.tsx
+++ src/components/imageGrid/components/gridElement.tsx
@@ -83,7 +83,7 @@ const GridElement = ({
       {/* Description */}
       <div className="px-6 py-4">
         <div className="mb-2 text-xl font-bold">{attraction.name}</div>
-        <p className="line-clamp-3 h-20 border border-red-300 text-base text-gray-700">
+        <p className="line-clamp-3 h-20 text-base text-gray-700">
           {attraction.description}
         </p>
       </div>
