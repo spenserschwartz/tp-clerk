746e8e559ce00df8495a11fd0ccc2d8ffe496e74
Spenser Schwartz <spensernet@gmail.com>
2023-10-19 15:22:09 -0700

    Update

--- src/components/features/components/TabItem.tsx
+++ src/components/features/components/TabItem.tsx
@@ -1,6 +1,6 @@
 import { Transition } from "@headlessui/react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 interface FeaturesTabProps {
   tab: number;
@@ -17,7 +17,7 @@ const FeaturesTabItem = ({ tab, tabs }: FeaturesTabProps) => {
     <Transition
       show={tab === 1}
       appear={true}
-      className="flex w-full justify-center border border-green-500"
+      className="flex w-full justify-center"
       enter="transition ease-in-out duration-700 transform order-first"
       enterFrom="opacity-0 translate-y-16"
       enterTo="opacity-100 translate-y-0"
@@ -27,9 +27,9 @@ const FeaturesTabItem = ({ tab, tabs }: FeaturesTabProps) => {
       beforeEnter={() => heightFix()}
       unmount={false}
     >
-      <div className="group relative flex h-96 items-end border border-red-500">
+      <div className="group relative flex h-96 items-end ">
         <Image
-          className="mx-auto rounded group-hover:blur md:max-w-none"
+          className="mx-auto rounded md:max-w-none md:group-hover:blur"
           src="https://travelperfect-bucket.s3.us-west-1.amazonaws.com/screenshot_quick_itinerary.png"
           width={0}
           height={0}
@@ -39,7 +39,7 @@ const FeaturesTabItem = ({ tab, tabs }: FeaturesTabProps) => {
         />
 
         {/* Button to anchor quickLaunch */}
-        <div className="animate-float invisible absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform justify-center rounded-md border border-green-400 group-hover:visible md:max-w-none">
+        <div className="animate-float invisible absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform justify-center rounded-md md:max-w-none md:group-hover:visible">
           <button
             type="button"
             className="rounded-md  bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
