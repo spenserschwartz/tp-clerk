b9d818277384484c4f6cfc47522455b2430617cc
Spenser Schwartz <spensernet@gmail.com>
2023-10-19 12:46:49 -0700

    Add comments

--- src/components/quickLaunch/index.tsx
+++ src/components/quickLaunch/index.tsx
@@ -133,18 +133,22 @@ const QuickLaunch = () => {
       {/* Display Generated Itinerary */}
       {parsedData.length ? (
         <div className="flex h-full max-w-5xl flex-col items-center">
+          {/* Button to create new itinerary */}
           <Button
             buttonText="Create new itinerary"
             buttonClickHandler={() => setParsedData([])}
           />
 
+          {/* Itinerary */}
           <div className="my-4 flex h-full flex-col overflow-y-scroll rounded-xl bg-gray-800 pr-2 shadow-xl sm:h-80">
             {parsedData.map((itineraryDay) => (
               <div key={`generatedAIMessage:${itineraryDay.dayOfWeek}`}>
+                {/* Date and day of week */}
                 <p className="text-font-bold mt-2 text-center text-xl text-orange-500">
                   {itineraryDay.date} - {itineraryDay.dayOfWeek}
                 </p>
 
+                {/* Morning, Afternoon, Evening */}
                 <ul className="ms-8 list-outside list-disc text-gray-300">
                   <li className="mb-1">Morning: {itineraryDay.morning}</li>
                   <li>Afternoon: {itineraryDay.afternoon}</li>
