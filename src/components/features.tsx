69b61a3b77c0d9c2b49b09371ca1e3a5d1706ec0
Spenser Schwartz <spensernet@gmail.com>
2023-10-19 12:31:11 -0700

    Turn aos back on

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -168,7 +168,7 @@ export default function Features() {
               <div className="transition-all">
                 <div
                   className="relative flex flex-col text-center lg:text-right"
-                  // data-aos="zoom-y-out"
+                  data-aos="zoom-y-out"
                   ref={tabs}
                 >
                   {/* Item 1 */}
