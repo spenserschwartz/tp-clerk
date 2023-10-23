47995a7347ba40298146943f138f6aefe797b8d3
Spenser Schwartz <spensernet@gmail.com>
2023-10-23 13:35:32 -0700

    Update tags

--- src/components/imageGrid/components/gridElement.tsx
+++ src/components/imageGrid/components/gridElement.tsx
@@ -115,7 +115,7 @@ const GridElement = ({
           #travel
         </span>
         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-          #winter
+          #attraction
         </span>
       </div>
     </div>
