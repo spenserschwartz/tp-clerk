f2472b6c915430b2da27ccbb1179a13abd419b97
Spenser Schwartz <spensernet@gmail.com>
2023-10-25 15:07:23 -0700

    AddIcon

--- public/icons/add.tsx
+++ public/icons/add.tsx
@@ -14,9 +14,9 @@ const AddIcon = () => {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 6v6m0 0v6m0-6h6m-6 0H6"
               />
             </svg>
