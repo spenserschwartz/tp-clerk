e046fc0f7ab93d4bba9be2dcf2b32dd87867df77
Spenser Schwartz <spensernet@gmail.com>
2023-10-31 13:58:06 -0700

    Add layout

--- src/pages/itinerary/[id].tsx
+++ src/pages/itinerary/[id].tsx
@@ -1,5 +1,14 @@
-const ItineraryPage = () => {
+import { type ReactElement } from "react";
+import { type NextPageWithLayout } from "../_app";
+
+import { RootLayout } from "~/components";
+
+const ItineraryPage: NextPageWithLayout = () => {
   return <div>Itinerary Page</div>;
+};
+
+ItineraryPage.getLayout = function getLayout(page: ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
 };
 
 export default ItineraryPage;
