07b74f6430c5320ee889f2991a70ae163789f6d1
Spenser Schwartz <spensernet@gmail.com>
2023-11-01 15:39:59 -0700

    useCreateItinerary

--- src/utils/hooks/useCreateItinerary.ts
+++ src/utils/hooks/useCreateItinerary.ts
@@ -26,3 +26,22 @@ const useCreateItinerary = () => {
 };
 
 export default useCreateItinerary;
+
+// const { mutate: createItinerary, isLoading: isCreatingItinerary } =
+//   api.itinerary.create.useMutation({
+//     onSuccess: () => {
+//       console.log("CreateItinerary Success");
+//     },
+//     onError: (e) => {
+//       const errorMessage = e.data?.zodError?.fieldErrors.content;
+//       if (errorMessage?.[0]) {
+//         toast.error(errorMessage[0]);
+//       } else {
+//         toast.error("Failed to create itinerary! Please try again later.");
+//       }
+//     },
+//     onSettled(data, error) {
+//       if (error) console.error(error);
+//       console.log("itinerary onSettled data", data);
+//     },
+//   });
