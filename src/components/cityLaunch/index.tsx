f4a28c49916b393fac8937ccd2cfbfe463c60071
Spenser Schwartz <spensernet@gmail.com>
2023-11-07 08:44:22 -0800

    cityLaunch

--- src/components/cityLaunch/index.tsx
+++ src/components/cityLaunch/index.tsx
@@ -80,56 +80,67 @@ const CityLaunch = ({ cityData }: CityLaunchProps) => {
       {(isLoadingAI || isCreatingItinerary) && <LoadingSection />}
       <div className="mx-auto w-full md:w-96 md:max-w-full">
         <div className="border border-gray-600  bg-gray-800 p-6 sm:rounded-md">
-          <form onSubmit={handleFormSubmit}>
-            {/* Destination */}
-            <div className="mb-6 block w-[300px] text-white">
-              <p className="text-gray-300">Included Attractions</p>
-              <ul className="">
-                {attractionsUpvotedByUser?.map((attraction) => {
-                  return <li key={attraction}>{attraction}</li>;
-                })}
-              </ul>
-            </div>
-
-            {/* Date Range Picker */}
-            <span className="text-gray-300">Choose your dates</span>
-            <DatePickerWithRange date={date} setDate={setDate} />
-
-            {/* Adventure Option */}
-            <div className="mt-4 flex flex-col">
-              <label className="fl">
-                <input
-                  name="adventureOption"
-                  type="checkbox"
-                  className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                />
-                <span className="ml-2 text-gray-300">
-                  Add some extra adventure!
-                </span>
-              </label>
-
-              <label className="fl">
-                <input
-                  name="adventureOption"
-                  type="checkbox"
-                  className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                />
-                <span className="ml-2 text-gray-300">
-                  Give some extra time to explore!
-                </span>
-              </label>
-            </div>
-
-            {/* Submit Button */}
-            <div className="mt-6 flex justify-center">
-              <button
-                type="submit"
-                className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
-              >
-                Make an itinerary!
+          {itineraryCreated ? (
+            <div className="flex flex-col">
+              <button className="focus:shadow-outline h-10 rounded-lg bg-green-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-green-800">
+                Go to itinerary
+              </button>
+              <button className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800">
+                New Itinerary
               </button>
             </div>
-          </form>
+          ) : (
+            <form onSubmit={handleFormSubmit}>
+              {/* Destination */}
+              <div className="mb-6 block w-[300px] text-white">
+                <p className="text-gray-300">Included Attractions</p>
+                <ul className="">
+                  {attractionsUpvotedByUser?.map((attraction) => {
+                    return <li key={attraction}>{attraction}</li>;
+                  })}
+                </ul>
+              </div>
+
+              {/* Date Range Picker */}
+              <span className="text-gray-300">Choose your dates</span>
+              <DatePickerWithRange date={date} setDate={setDate} />
+
+              {/* Adventure Option */}
+              <div className="mt-4 flex flex-col">
+                <label className="fl">
+                  <input
+                    name="adventureOption"
+                    type="checkbox"
+                    className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
+                  />
+                  <span className="ml-2 text-gray-300">
+                    Add some extra adventure!
+                  </span>
+                </label>
+
+                <label className="fl">
+                  <input
+                    name="adventureOption"
+                    type="checkbox"
+                    className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
+                  />
+                  <span className="ml-2 text-gray-300">
+                    Give some extra time to explore!
+                  </span>
+                </label>
+              </div>
+
+              {/* Submit Button */}
+              <div className="mt-6 flex justify-center">
+                <button
+                  type="submit"
+                  className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
+                >
+                  Make an itinerary!
+                </button>
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </div>
