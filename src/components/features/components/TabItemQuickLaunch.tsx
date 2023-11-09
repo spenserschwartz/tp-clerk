b5e8b9451eba7f9779f4759a9444338f582edd62
Spenser Schwartz <spensernet@gmail.com>
2023-11-09 10:01:29 -0800

    Mobile classes to cover hover and click effects

--- src/components/features/components/TabItemQuickLaunch.tsx
+++ src/components/features/components/TabItemQuickLaunch.tsx
@@ -1,5 +1,6 @@
 import { Transition } from "@headlessui/react";
 import Image from "next/image";
+import { useState } from "react";
 
 import { screenshots } from "~/utils/images";
 
@@ -9,6 +10,9 @@ interface FeaturesTabProps {
 }
 
 const TabItemQuickLaunch = ({ tab, tabs }: FeaturesTabProps) => {
+  const [isClicked, setIsClicked] = useState(false); // for mobile
+  const handleClick = () => setIsClicked(!isClicked);
+
   const heightFix = () => {
     if (tabs.current?.parentElement)
       tabs.current.parentElement.style.height = `${tabs.current.clientHeight}px`;
@@ -28,9 +32,11 @@ const TabItemQuickLaunch = ({ tab, tabs }: FeaturesTabProps) => {
       beforeEnter={() => heightFix()}
       unmount={false}
     >
-      <div className="group relative flex h-96 items-end ">
+      <div className="group relative flex h-96 items-end" onClick={handleClick}>
         <Image
-          className="mx-auto rounded md:max-w-none md:group-hover:blur"
+          className={`mx-auto rounded group-hover:blur md:max-w-none ${
+            isClicked ? "blur" : "" // should be blurred if clicked (for mobile) or hovered
+          }`}
           src={screenshots.quickItinerary}
           width={0}
           height={0}
@@ -40,7 +46,11 @@ const TabItemQuickLaunch = ({ tab, tabs }: FeaturesTabProps) => {
         />
 
         {/* Button to anchor quickLaunch */}
-        <div className="animate-float invisible absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform justify-center rounded-md md:max-w-none md:group-hover:visible">
+        <div
+          className={`animate-float ${
+            isClicked ? "" : "invisible" // should be invisible if not clicked (for mobile) or hovered
+          } absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform justify-center rounded-md group-hover:visible md:max-w-none`}
+        >
           <button
             type="button"
             className="rounded-md  bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
