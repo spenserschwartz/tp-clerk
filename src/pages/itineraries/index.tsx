c3ad1471cc07eeae3cdf8312e22e8daf41bd65b8
Spenser Schwartz <spensernet@gmail.com>
2023-11-14 14:27:07 -0800

    Show itinerary ids

--- src/pages/itineraries/index.tsx
+++ src/pages/itineraries/index.tsx
@@ -2,6 +2,7 @@ import { type ReactElement } from "react";
 import { type NextPageWithLayout } from "~/types/pages";
 import { api } from "~/utils/api";
 
+import Link from "next/link";
 import { RootLayout } from "~/components";
 
 const ItinerariesPage: NextPageWithLayout = () => {
@@ -9,7 +10,19 @@ const ItinerariesPage: NextPageWithLayout = () => {
 
   console.log("allItineraries", allItineraries);
 
-  return <div>ItinerariesPage</div>;
+  return (
+    <main className="flex justify-center">
+      <ul className="list-inside list-disc">
+        {allItineraries?.map((itinerary) => {
+          return (
+            <li key={itinerary.id}>
+              <Link href={`/itinerary/${itinerary.id}`}>{itinerary.id}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 };
 
 ItinerariesPage.getLayout = function getLayout(page: ReactElement) {
