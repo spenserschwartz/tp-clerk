eb936cc7f855be4e824bac74c9ef28d40476cc3c
Spenser Schwartz <spensernet@gmail.com>
2023-11-16 14:11:21 -0800

    ItineraryGridElement gets details

--- src/components/itineraryImageGrid/components/gridElement.tsx
+++ src/components/itineraryImageGrid/components/gridElement.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { ParsedAIMessageInterface } from "~/types";
+import type { ParsedAIMessageInterface } from "~/types";
 
 import { type ItineraryWithCityInfoType } from "~/types/router";
 
@@ -10,7 +10,12 @@ interface ItineraryGridElementProps {
 
 const ItineraryGridElement = ({ itinerary }: ItineraryGridElementProps) => {
   const details = itinerary.details as unknown as ParsedAIMessageInterface[];
+  const {
+    city: { imageURL: cityImageURL, name: cityName },
+  } = itinerary;
   const { length: numberOfDays } = details;
+
+  const itineraryName = `${numberOfDays} days in ${cityName}`;
 
   console.log("numberOfDays", numberOfDays);
 
@@ -22,7 +27,7 @@ const ItineraryGridElement = ({ itinerary }: ItineraryGridElementProps) => {
       {/* Image */}
       <div className="group aspect-h-7 aspect-w-10 relative block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
         <Image
-          src={"/images/placeholder.png"}
+          src={(cityImageURL as string) ?? "/images/placeholder.png"}
           alt=""
           className="pointer-events-none object-cover"
           width={100}
@@ -36,7 +41,7 @@ const ItineraryGridElement = ({ itinerary }: ItineraryGridElementProps) => {
       <div className="px-4 py-2">
         {/* Itinerary Name */}
         <div className="flex h-14 items-center justify-center text-center text-xl font-bold">
-          Itinerary name here
+          {itineraryName}
         </div>
 
         {/* Description */}
@@ -53,7 +58,7 @@ const ItineraryGridElement = ({ itinerary }: ItineraryGridElementProps) => {
           #travel
         </span>
         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-          #attraction
+          {`#${cityName}`}
         </span>
         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
           #itinerary
