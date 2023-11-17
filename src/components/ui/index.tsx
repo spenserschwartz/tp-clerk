5b28526260a1a16b48226c5809227e62cbc51091
Spenser Schwartz <spensernet@gmail.com>
2023-11-17 11:32:22 -0800

    barrel importing

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,7 @@
+import Breadcrumbs from "./breadcrumbs";
+import Footer from "./footer";
+import Header from "./header";
+import Logo from "./logo";
+import MobileMenu from "./mobile-menu";
+
+export { Breadcrumbs, Footer, Header, Logo, MobileMenu };
