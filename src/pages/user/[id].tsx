2f7c00d160283d4a6241fc9a3b4d84fde3c3858e
Spenser Schwartz <spensernet@gmail.com>
2023-11-17 10:38:08 -0800

    page title

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -18,11 +18,16 @@ const UserPage: NextPageWithLayout<{ userId: string }> = ({ userId }) => {
 
   console.log("itinerariesByUser", itinerariesByUser);
 
+  const pageTitle =
+    user?.firstName && user?.lastName
+      ? `${user.firstName} ${user.lastName}`
+      : userId;
+
   return (
     <main className="flex w-full flex-col items-center px-2">
       <div className="relative flex w-full items-center justify-center">
         <h1 className="my-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          {userId}
+          {pageTitle}
         </h1>
       </div>
       <ItineraryImageGrid
