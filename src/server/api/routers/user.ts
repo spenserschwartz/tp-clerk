9e6456b2c16e159ce01be48eff6e9613f9e31d42
Spenser Schwartz <spensernet@gmail.com>
2023-11-27 14:45:05 -0800

    userRouter only has getAll

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,19 +1,4 @@
-import { TRPCError } from "@trpc/server";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-import { z } from "zod";
-import {
-  createTRPCRouter,
-  privateProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
-
-// Create a new ratelimiter, that allows 5 requests per 1 minute
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "1 m"),
-  analytics: true,
-});
+import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
   getAll: privateProcedure.query(async ({ ctx }) => {
@@ -24,25 +9,6 @@ export const userRouter = createTRPCRouter({
 
     return users;
   }),
-
-  create: publicProcedure
-    .input(z.object({ id: z.string(), image: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      const userId = ctx.userId ?? "not-logged-in";
-      console.log("CREATE userId", userId);
-
-      const { success } = await ratelimit.limit(userId);
-      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-
-      const newItinerary = await ctx.prisma.user.create({
-        data: {
-          id: input.id,
-          image: input.image,
-        },
-      });
-
-      return newItinerary;
-    }),
 
   // More routers here...
 });
