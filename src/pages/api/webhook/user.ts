d3b57d04bc7b15f7977ba6dca13be443a15c3200
Spenser Schwartz <spensernet@gmail.com>
2023-11-27 14:45:38 -0800

    webhook successfully creates user on user.created

--- src/pages/api/webhook/user.ts
+++ src/pages/api/webhook/user.ts
@@ -8,13 +8,7 @@ type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
 };
 
-enum EventTypes {
-  "userCreated" = "user.created",
-  "userUpdated" = "user.updated",
-  "userDeleted" = "user.deleted",
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
@@ -31,7 +25,6 @@ export default function handler(
   }
 
   const evt = req.body as WebhookEvent;
-
   const svixHeaders: WebhookRequiredHeaders = {
     "svix-id": req.headers["svix-id"],
     "svix-timestamp": req.headers["svix-timestamp"],
@@ -39,8 +32,6 @@ export default function handler(
   };
 
   const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET ?? "");
-
-  console.log("DO SOMETHING");
 
   try {
     wh.verify(JSON.stringify(req.body), svixHeaders);
@@ -50,25 +41,44 @@ export default function handler(
       .json({ message: "The verification of this request failed." });
   }
 
-  console.log("MADE IT TO SWITCH");
+  const { id } = evt.data;
+  const eventType = evt.type;
 
-  switch (evt.type) {
-    case EventTypes.userCreated: {
-      // user created logic
-      console.log("USER CREATED EVENT");
+  const userData = evt.data as { id: string; image_url: string };
+  const { image_url } = userData;
 
-      // Get the ID and type
-      const { id } = evt.data;
-      const eventType = evt.type;
-      console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-      console.log("This is evt", evt);
+  if (eventType === "user.created" || eventType === "user.updated") {
+    console.log("USER CREATED EVENT");
+    console.log("eventType:", eventType);
+
+    // try {
+    //   await prisma.user
+    //     .create({
+    //       data: { id: id, imageURL: image_url },
+    //     })
+    // .then(() =>
+    //   res.status(200).json({ message: "User created successfully" })
+    // );
+    // } catch (error) {
+    //   console.error("Error creating user:", error);
+    //   return res.status(500).json({ error: "Error creating user" });
+    // }
+
+    try {
+      await prisma.user
+        .upsert({
+          where: { id: id },
+          update: { imageURL: image_url },
+          create: { id: id, imageURL: image_url },
+        })
+        .then(() =>
+          res.status(200).json({ message: "User upserted successfully" })
+        );
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return res.status(500).json({ error: "Error creating user" });
     }
-
-    case EventTypes.userUpdated: {
-      // user updated logic
-      console.log("USER UPDATED EVENT");
-    }
-    default:
-      return res.status(200).json({});
   }
+
+  return res.status(200).json({});
 }
