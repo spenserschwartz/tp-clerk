08426ed82aae560dd15cc58fe44b2e7e22d57d8c
Spenser Schwartz <spensernet@gmail.com>
2023-11-29 15:59:22 -0800

    viewFinderCircleIcon

--- src/components/dropdownMenu/index.tsx
+++ src/components/dropdownMenu/index.tsx
@@ -1,8 +1,8 @@
 import { Menu } from "@headlessui/react";
 import {
   PencilIcon,
-  PencilSquareIcon,
   TrashIcon,
+  ViewfinderCircleIcon,
 } from "@heroicons/react/20/solid";
 
 const DropdownMenu = () => {
@@ -21,7 +21,7 @@ const DropdownMenu = () => {
                   active ? "bg-gray-100 text-gray-900" : "text-gray-700"
                 } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
               >
-                <PencilSquareIcon
+                <ViewfinderCircleIcon
                   className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
                   aria-hidden="true"
                 />
