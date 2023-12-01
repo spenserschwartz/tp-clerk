5b54d6d198259de1bd500e475317e6043d24cdac
Spenser Schwartz <spensernet@gmail.com>
2023-12-01 12:41:44 -0800

    Show all itineraries on page

--- src/pages/itineraries/index.tsx
+++ src/pages/itineraries/index.tsx
@@ -2,25 +2,19 @@ import { type ReactElement } from "react";
 import { type NextPageWithLayout } from "~/types/pages";
 import { api } from "~/utils/api";
 
-import Link from "next/link";
-import { RootLayout } from "~/components";
+import { ItineraryImageGrid, RootLayout } from "~/components";
 
 const ItinerariesPage: NextPageWithLayout = () => {
   const { data: allItineraries } = api.itinerary.getAllWithCityInfo.useQuery();
 
-  console.log("allItineraries", allItineraries);
-
   return (
-    <main className="flex justify-center">
-      <ul className="list-inside list-disc">
-        {allItineraries?.map((itinerary) => {
-          return (
-            <li key={itinerary.id}>
-              <Link href={`/itinerary/${itinerary.id}`}>{itinerary.id}</Link>
-            </li>
-          );
-        })}
-      </ul>
+    <main className="flex w-full flex-col items-center px-2">
+      <div className="relative flex w-full items-center justify-center">
+        <h1 className="my-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          Itineraries
+        </h1>
+      </div>
+      <ItineraryImageGrid itineraries={allItineraries ?? []} />
     </main>
   );
 };
