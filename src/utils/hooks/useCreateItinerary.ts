c2f1ee75da459a863c4877a63649fca1c6030e45
Spenser Schwartz <spensernet@gmail.com>
2023-12-04 15:38:27 -0800

    success toast

--- src/utils/hooks/useCreateItinerary.ts
+++ src/utils/hooks/useCreateItinerary.ts
@@ -9,7 +9,7 @@ const useCreateItinerary = () => {
     data: itineraryData,
   } = api.itinerary.create.useMutation({
     onSuccess: () => {
-      console.log("CreateItinerary Success");
+      toast.success("Itinerary created!");
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
@@ -34,22 +34,3 @@ const useCreateItinerary = () => {
 };
 
 export default useCreateItinerary;
-
-// const { mutate: createItinerary, isLoading: isCreatingItinerary } =
-//   api.itinerary.create.useMutation({
-//     onSuccess: () => {
-//       console.log("CreateItinerary Success");
-//     },
-//     onError: (e) => {
-//       const errorMessage = e.data?.zodError?.fieldErrors.content;
-//       if (errorMessage?.[0]) {
-//         toast.error(errorMessage[0]);
-//       } else {
-//         toast.error("Failed to create itinerary! Please try again later.");
-//       }
-//     },
-//     onSettled(data, error) {
-//       if (error) console.error(error);
-//       console.log("itinerary onSettled data", data);
-//     },
-//   });
