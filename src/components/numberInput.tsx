d61c5f41bb80ac2ae5b1452f7ed8a542375f429c
Spenser Schwartz <spensernet@gmail.com>
2023-12-04 13:10:50 -0800

    add comments

--- src/components/numberInput.tsx
+++ src/components/numberInput.tsx
@@ -26,6 +26,7 @@ const NumberInput = ({
       data-hs-input-number
     >
       <div className="flex items-center gap-x-1.5">
+        {/* Minus Button */}
         <button
           type="button"
           className="inline-flex h-6 w-6 items-center justify-center gap-x-2 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -46,12 +47,16 @@ const NumberInput = ({
             <path d="M5 12h14" />
           </svg>
         </button>
+
+        {/* Input */}
         <input
           className="w-6 border-0 bg-transparent p-0 text-center text-gray-800 focus:ring-0 dark:text-white"
           type="text"
           value={value}
           onChange={inputChangeHandler}
         />
+
+        {/* Plus Button */}
         <button
           type="button"
           className="inline-flex h-6 w-6 items-center justify-center gap-x-2 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
