54b0dc9b55f22ab15ad53d81521289b58f03f940
Spenser Schwartz <spensernet@gmail.com>
2023-12-04 12:11:38 -0800

    div tag to main tag

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -25,8 +25,8 @@ export const RootLayout = (props: PropsWithChildren) => {
         {/* Spacing between header and main content */}
         <div className="mb-24 md:mb-32" />
 
-        {/* Content */}
-        <div className="px-10">{props.children}</div>
+        {/* Main Content */}
+        <main className="px-10">{props.children}</main>
 
         {/* Grow the page so that footer is at bottom of page if there is no scroll */}
         <div className="flex-grow"></div>
