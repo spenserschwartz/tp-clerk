0d6e7c90b65ffdeaca3d4b9472b205e91c6a0d6b
Spenser Schwartz <spensernet@gmail.com>
2023-12-05 14:25:57 -0800

    access user page

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,12 @@ export default authMiddleware({
     "/sign-in/[[...index]]",
     "/city/:slug", // Access city page without logging in
     "/itinerary/:id", // Access itinerary page without logging in
+    "/user/:id", // Access user page without logging in
 
     "/api/trpc/:path+", // Public API to allow TRPC handle route protection (via publicProcedure, privateProcedure, etc.)
     "/api/chat:additionalDetails*", // Chatbot
-
     "/api/webhook/user", // Webhooks
     "/api/webhook(.*)", // Open any webhooks up
-    "api/test",
   ],
 });
 
