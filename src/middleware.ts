2d7dc397450c405f0c9c549a5ef4050e224f2a38
Spenser Schwartz <spensernet@gmail.com>
2023-12-05 16:05:59 -0800

    add quick-launch

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ export default authMiddleware({
     "/city/:slug", // Access city page without logging in
     "/itinerary/:id", // Access itinerary page without logging in
     "/user/:id", // Access user page without logging in
+    "/quick-launch",
 
     "/api/trpc/:path+", // Public API to allow TRPC handle route protection (via publicProcedure, privateProcedure, etc.)
     "/api/chat:additionalDetails*", // Chatbot
