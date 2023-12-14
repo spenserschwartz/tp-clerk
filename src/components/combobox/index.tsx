1182b6f0464c1c168496b6ddd47d030771bbd8ec
Spenser Schwartz <spensernet@gmail.com>
2023-12-14 12:50:11 -0800

    combobox (not used)

--- src/components/combobox/index.tsx
+++ src/components/combobox/index.tsx
@@ -14,7 +14,7 @@ interface ComboboxOptionsType {
 }
 
 interface ComboboxProps {
-  options: ComboboxOptionsType[];
+  options?: ComboboxOptionsType[];
 }
 
 export default function Combobox({ options }: ComboboxProps) {
@@ -55,7 +55,7 @@ export default function Combobox({ options }: ComboboxProps) {
           />
         </HeadlessCombobox.Button>
 
-        {options?.length > 0 && (
+        {options && (
           <HeadlessCombobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {options?.map((option) => (
               <HeadlessCombobox.Option
