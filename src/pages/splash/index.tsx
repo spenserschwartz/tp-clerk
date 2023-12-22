4202064178995fb51dfaf9075186c60aa80fbd32
Spenser Schwartz <spensernet@gmail.com>
2023-12-22 10:38:02 -0800

    splash

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,13 +1,6 @@
-import { useChat } from "ai/react";
-import GooglePlaces from "~/components/google/places";
-
 const SplashPage = () => {
-  const { messages, handleSubmit, input, handleInputChange } = useChat();
-
   return (
-    <div className="h-screen w-screen border-2 border-red-400">
-      <GooglePlaces />
-    </div>
+    <div className="h-screen w-screen border-2 border-red-400">Splash</div>
   );
 };
 
