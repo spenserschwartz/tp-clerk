319014be0aa80396e3b541e7da2b597c1d4a096a
Spenser Schwartz <spensernet@gmail.com>
2024-01-08 08:05:41 -0800

    framer-motion >> ZoomInWrapper

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import ZoomInWrapper from "./framer-motion/zoom-in";
 
 interface CarouselProps {
   slides: string[];
@@ -29,57 +30,55 @@ export default function Carousel({ slides }: CarouselProps) {
   }, [nextSlide]);
 
   return (
-    <div
-      className="relative overflow-hidden"
-      data-aos="zoom-in"
-      data-aos-delay="500"
-    >
-      <div
-        className={`flex transition duration-500 ease-out`}
-        style={{
-          transform: `translateX(-${current * 100}%)`,
-        }}
-      >
-        {slides.map((slide) => {
-          //   return <img src={s} />;
-          return (
-            <Image
-              className="rounded-xl shadow-md"
-              key={`Carousel_Image-${slide}`}
-              src={slide || "/images/placeholder.png"}
-              alt={`Carousel_Image-${slide}`}
-              width={800}
-              height={800}
-              priority
-            />
-          );
-        })}
-      </div>
+    <ZoomInWrapper>
+      <div className="relative overflow-hidden">
+        <div
+          className={`flex transition duration-500 ease-out`}
+          style={{
+            transform: `translateX(-${current * 100}%)`,
+          }}
+        >
+          {slides.map((slide) => {
+            //   return <img src={s} />;
+            return (
+              <Image
+                className="rounded-xl shadow-md"
+                key={`Carousel_Image-${slide}`}
+                src={slide || "/images/placeholder.png"}
+                alt={`Carousel_Image-${slide}`}
+                width={800}
+                height={800}
+                priority
+              />
+            );
+          })}
+        </div>
 
-      <div className="absolute top-0 flex h-full w-full items-center justify-between px-10 text-3xl text-white">
-        <button onClick={previousSlide}>
-          <ArrowLeftCircleIcon />
-        </button>
-        <button onClick={nextSlide}>
-          <ArrowRightCircleIcon />
-        </button>
-      </div>
+        <div className="absolute top-0 flex h-full w-full items-center justify-between px-10 text-3xl text-white">
+          <button onClick={previousSlide}>
+            <ArrowLeftCircleIcon />
+          </button>
+          <button onClick={nextSlide}>
+            <ArrowRightCircleIcon />
+          </button>
+        </div>
 
-      <div className="absolute bottom-0 flex w-full justify-center gap-3 py-4">
-        {slides.map((s, i) => {
-          return (
-            <div
-              onClick={() => {
-                setCurrent(i);
-              }}
-              key={"circle" + i}
-              className={`h-3 w-3 cursor-pointer rounded-full  ${
-                i == current ? "bg-white" : "bg-gray-500"
-              }`}
-            ></div>
-          );
-        })}
+        <div className="absolute bottom-0 flex w-full justify-center gap-3 py-4">
+          {slides.map((s, i) => {
+            return (
+              <div
+                onClick={() => {
+                  setCurrent(i);
+                }}
+                key={"circle" + i}
+                className={`h-3 w-3 cursor-pointer rounded-full  ${
+                  i == current ? "bg-white" : "bg-gray-500"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </ZoomInWrapper>
   );
 }
