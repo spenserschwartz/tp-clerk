4722ade0b3fe989575bd8fef8bad4a98d17419a6
Spenser Schwartz <spensernet@gmail.com>
2024-01-08 07:20:29 -0800

    remove aos

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,9 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect, type PropsWithChildren } from "react";
-
+import { type PropsWithChildren } from "react";
 import { Footer, Header } from "~/components/ui";
 
 // Todo: add font-inter to global styles
 
 export const RootLayout = (props: PropsWithChildren) => {
-  // AOS, animate on scroll
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: "phone",
-      duration: 700,
-      easing: "ease-out-cubic",
-    });
-  });
-
   return (
     <div className="bg-white tracking-tight text-gray-900 antialiased">
       <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
