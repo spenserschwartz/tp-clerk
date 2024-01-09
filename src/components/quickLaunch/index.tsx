02eb378aec70367139a6e52354cbe1dd8257ae24
Spenser Schwartz <spensernet@gmail.com>
2024-01-09 12:01:02 -0800

    remove FadeUpWrapper

--- src/components/quickLaunch/index.tsx
+++ src/components/quickLaunch/index.tsx
@@ -19,7 +19,6 @@ import { RequestOptionType, type PlaceResult } from "~/types/google";
 import { DatePickerWithRange } from "~/ui/datePickerWithRange";
 import { createRequestOptions } from "~/utils/common";
 import { useAIGenerateItinerary, useCreateItinerary } from "~/utils/hooks";
-import { FadeUpWrapper } from "../framer-motion";
 import { quickLaunchCities, unknownClerkCity } from "../utils";
 
 const libraries: Libraries = ["places"];
@@ -146,124 +145,122 @@ const QuickLaunch = () => {
 
   return (
     isLoaded && (
-      <FadeUpWrapper>
-        <div className="my-8 flex h-full flex-col items-center">
-          {/* Loading Page */}
-          {(isLoadingAI || isCreatingItinerary) && <LoadingSection />}
+      <div className="my-8 flex h-full flex-col items-center">
+        {/* Loading Page */}
+        {(isLoadingAI || isCreatingItinerary) && <LoadingSection />}
 
-          {/* Quick Launch Form */}
-          {!isLoadingAI && !parsedData.length && (
-            <div className="mx-auto w-full md:w-96 md:max-w-full">
-              <div className="border border-gray-600  bg-gray-800 p-6 sm:rounded-md">
-                <form onSubmit={handleFormSubmit}>
-                  {/* Destination */}
+        {/* Quick Launch Form */}
+        {!isLoadingAI && !parsedData.length && (
+          <div className="mx-auto w-full md:w-96 md:max-w-full">
+            <div className="border border-gray-600  bg-gray-800 p-6 sm:rounded-md">
+              <form onSubmit={handleFormSubmit}>
+                {/* Destination */}
 
-                  <div className="mb-6 block w-[300px] font-medium leading-6 text-gray-300">
-                    <p className="flex justify-between">
-                      <span>Where do you want to go?</span>
-                      <span
-                        className="cursor-pointer text-blue-600 hover:text-blue-700"
-                        onClick={() => setCustomCity(!customCity)}
-                      >
-                        {customCity ? "Prefilled" : "Custom"}
-                      </span>
-                    </p>
-
-                    {!customCity ? (
-                      <Select
-                        options={quickLaunchCities}
-                        selected={chosenCityName}
-                        setSelected={setChosenCityName}
-                      />
-                    ) : (
-                      <div>
-                        <PlacesAutoComplete
-                          setSelected={setChosenCustomCity}
-                          requestOptions={requestOptions}
-                        />
-                      </div>
-                    )}
-                  </div>
-
-                  {/* Date Range Picker */}
-                  <span className="text-gray-300">Choose your dates</span>
-                  <DatePickerWithRange date={date} setDate={setDate} />
-
-                  {/* Adventure Option */}
-                  <div className="mt-4 flex flex-col">
-                    <label className="fl">
-                      <input
-                        name="adventureOption"
-                        type="checkbox"
-                        className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                      />
-                      <span className="ml-2 text-gray-300">
-                        Add some extra adventure!
-                      </span>
-                    </label>
-
-                    <label className="fl">
-                      <input
-                        name="adventureOption"
-                        type="checkbox"
-                        className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
-                      />
-                      <span className="ml-2 text-gray-300">
-                        Give some extra time to explore!
-                      </span>
-                    </label>
-                  </div>
-
-                  {/* Submit Button */}
-                  <div className="mt-6 flex justify-center">
-                    <button
-                      type="submit"
-                      className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
+                <div className="mb-6 block w-[300px] font-medium leading-6 text-gray-300">
+                  <p className="flex justify-between">
+                    <span>Where do you want to go?</span>
+                    <span
+                      className="cursor-pointer text-blue-600 hover:text-blue-700"
+                      onClick={() => setCustomCity(!customCity)}
                     >
-                      Make an itinerary!
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          )}
+                      {customCity ? "Prefilled" : "Custom"}
+                    </span>
+                  </p>
 
-          {/* Display Generated Itinerary */}
-          {parsedData.length > 0 && (
-            <div className="flex flex-col items-center">
-              <div className="flex">
-                {/* Button to create new itinerary */}
-                <Button
-                  buttonText="Create new itinerary"
-                  buttonClickHandler={() => setParsedData([])}
-                  size="xl"
-                />
+                  {!customCity ? (
+                    <Select
+                      options={quickLaunchCities}
+                      selected={chosenCityName}
+                      setSelected={setChosenCityName}
+                    />
+                  ) : (
+                    <div>
+                      <PlacesAutoComplete
+                        setSelected={setChosenCustomCity}
+                        requestOptions={requestOptions}
+                      />
+                    </div>
+                  )}
+                </div>
 
-                {!itineraryCreated && (
+                {/* Date Range Picker */}
+                <span className="text-gray-300">Choose your dates</span>
+                <DatePickerWithRange date={date} setDate={setDate} />
+
+                {/* Adventure Option */}
+                <div className="mt-4 flex flex-col">
+                  <label className="fl">
+                    <input
+                      name="adventureOption"
+                      type="checkbox"
+                      className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
+                    />
+                    <span className="ml-2 text-gray-300">
+                      Add some extra adventure!
+                    </span>
+                  </label>
+
+                  <label className="fl">
+                    <input
+                      name="adventureOption"
+                      type="checkbox"
+                      className="rounded-full border-gray-600 bg-transparent  placeholder-gray-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:ring-offset-0"
+                    />
+                    <span className="ml-2 text-gray-300">
+                      Give some extra time to explore!
+                    </span>
+                  </label>
+                </div>
+
+                {/* Submit Button */}
+                <div className="mt-6 flex justify-center">
                   <button
-                    className="ml-4 rounded bg-green-500 px-3.5 py-3 text-xs font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                    onClick={saveItineraryHandler}
+                    type="submit"
+                    className="focus:shadow-outline h-10 rounded-lg bg-indigo-700 px-5 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
                   >
-                    Save Itinerary
+                    Make an itinerary!
                   </button>
-                )}
-
-                {itineraryCreated && (
-                  <button
-                    className="ml-4 rounded bg-blue-500 px-3.5 py-3 text-xs font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                    onClick={() =>
-                      void router.push(`/itinerary/${itineraryData?.id}`)
-                    }
-                  >
-                    Go to Itinerary
-                  </button>
-                )}
-              </div>
-              <Itinerary parsedData={parsedData} />
+                </div>
+              </form>
             </div>
-          )}
-        </div>
-      </FadeUpWrapper>
+          </div>
+        )}
+
+        {/* Display Generated Itinerary */}
+        {parsedData.length > 0 && (
+          <div className="flex flex-col items-center">
+            <div className="flex">
+              {/* Button to create new itinerary */}
+              <Button
+                buttonText="Create new itinerary"
+                buttonClickHandler={() => setParsedData([])}
+                size="xl"
+              />
+
+              {!itineraryCreated && (
+                <button
+                  className="ml-4 rounded bg-green-500 px-3.5 py-3 text-xs font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  onClick={saveItineraryHandler}
+                >
+                  Save Itinerary
+                </button>
+              )}
+
+              {itineraryCreated && (
+                <button
+                  className="ml-4 rounded bg-blue-500 px-3.5 py-3 text-xs font-semibold text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  onClick={() =>
+                    void router.push(`/itinerary/${itineraryData?.id}`)
+                  }
+                >
+                  Go to Itinerary
+                </button>
+              )}
+            </div>
+            <Itinerary parsedData={parsedData} />
+          </div>
+        )}
+      </div>
     )
   );
 };
