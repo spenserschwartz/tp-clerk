346b385f11c39089ae849581f97be9e881514f6e
Spenser Schwartz <spensernet@gmail.com>
2024-01-09 11:58:15 -0800

    FadeUpWrapper

--- src/components/itineraryImageGrid/components/gridElement.tsx
+++ src/components/itineraryImageGrid/components/gridElement.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 import DropdownMenu from "~/components/dropdownMenu";
+import { FadeUpWrapper } from "~/components/framer-motion";
 import type { ParsedAIMessageInterface } from "~/types";
 import type { ItineraryWithCityInfoType } from "~/types/router";
 
@@ -29,69 +30,68 @@ const ItineraryGridElement = ({ itinerary }: ItineraryGridElementProps) => {
   };
 
   return (
-    <div
-      className="max-w-sm overflow-hidden rounded shadow-lg"
-      data-aos="fade-up"
-    >
-      {/* Image & pencil icon*/}
-      <div
-        className="group aspect-h-7 aspect-w-10 relative block w-full cursor-pointer overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
-        onClick={gridElementClickHandler}
-      >
-        {/* Pencil icon in top right of image */}
+    <FadeUpWrapper>
+      <div className="max-w-sm overflow-hidden rounded shadow-lg">
+        {/* Image & pencil icon*/}
         <div
-          className="absolute bottom-auto left-auto z-10 h-auto w-auto p-2"
-          onClick={(e) => e.stopPropagation()} // prevents gridElementClickHandler in parent from being called
+          className="group aspect-h-7 aspect-w-10 relative block w-full cursor-pointer overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
+          onClick={gridElementClickHandler}
         >
-          <DropdownMenu
-            itineraryID={itinerary.id}
-            itineraryUserID={itineraryUserId}
+          {/* Pencil icon in top right of image */}
+          <div
+            className="absolute bottom-auto left-auto z-10 h-auto w-auto p-2"
+            onClick={(e) => e.stopPropagation()} // prevents gridElementClickHandler in parent from being called
+          >
+            <DropdownMenu
+              itineraryID={itinerary.id}
+              itineraryUserID={itineraryUserId}
+            />
+          </div>
+
+          {/* Image */}
+          <Image
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            src={itineraryImageURL ?? cityImageURL ?? "/images/placeholder.png"}
+            alt=""
+            className="pointer-events-none object-cover"
+            width={100}
+            height={100}
+            priority
+            unoptimized
           />
         </div>
 
-        {/* Image */}
-        <Image
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          src={itineraryImageURL ?? cityImageURL ?? "/images/placeholder.png"}
-          alt=""
-          className="pointer-events-none object-cover"
-          width={100}
-          height={100}
-          priority
-          unoptimized
-        />
-      </div>
+        {/* Details */}
+        <div className="px-4 py-2">
+          {/* Itinerary Name */}
+          <div className="flex h-14 items-center justify-center text-center text-xl font-bold">
+            <p
+              className="cursor-pointer hover:opacity-75"
+              onClick={gridElementClickHandler}
+            >
+              {itineraryTitle ?? itineraryName}
+            </p>
+          </div>
 
-      {/* Details */}
-      <div className="px-4 py-2">
-        {/* Itinerary Name */}
-        <div className="flex h-14 items-center justify-center text-center text-xl font-bold">
-          <p
-            className="cursor-pointer hover:opacity-75"
-            onClick={gridElementClickHandler}
-          >
-            {itineraryTitle ?? itineraryName}
-          </p>
+          {/* Description */}
+          <div>
+            <p className="line-clamp-3 h-20  py-2 text-base text-gray-700">
+              {itineraryDescription}
+            </p>
+          </div>
         </div>
 
-        {/* Description */}
-        <div>
-          <p className="line-clamp-3 h-20  py-2 text-base text-gray-700">
-            {itineraryDescription}
-          </p>
+        {/* Tags */}
+        <div className="px-6 pb-2 pt-4">
+          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+            {`#${cityName}`}
+          </span>
+          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+            #itinerary
+          </span>
         </div>
       </div>
-
-      {/* Tags */}
-      <div className="px-6 pb-2 pt-4">
-        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-          {`#${cityName}`}
-        </span>
-        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-          #itinerary
-        </span>
-      </div>
-    </div>
+    </FadeUpWrapper>
   );
 };
 
