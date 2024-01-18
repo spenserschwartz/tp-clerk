16e2279077929f42c0a98fcc978724d9635468ac
Spenser Schwartz <spensernet@gmail.com>
2024-01-18 07:59:58 -0800

    EditorContent editable

--- src/components/Itinerary/components/Notes.tsx
+++ src/components/Itinerary/components/Notes.tsx
@@ -3,8 +3,17 @@ import { TextEditor } from "~/components";
 
 const ItineraryNotes = () => {
   const [editable, setEditable] = useState(false);
+
+  console.log("editable", editable);
   return (
     <div className="mt-10">
+      <button
+        className={`rounded bg-blue-600 px-4 py-1 text-white hover:bg-blue-700`}
+        onClick={() => setEditable(true)}
+      >
+        Edit Notes
+      </button>
+
       <TextEditor editable={editable} />
     </div>
   );
--- src/components/tiptap/Editor.tsx
+++ src/components/tiptap/Editor.tsx
@@ -10,59 +10,62 @@ interface TextEditorProps {
 }
 
 const TextEditor = ({ editable }: TextEditorProps) => {
-  const editor = useEditor({
-    editable,
-    extensions: [
-      StarterKit,
-      Link.configure({
-        linkOnPaste: false,
-        protocols: [
-          {
-            scheme: "tel",
-            optionalSlashes: true,
-          },
-        ],
-      }),
-      Underline,
-    ],
-    editorProps: {
-      attributes: {
-        class:
-          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2",
+  const editor = useEditor(
+    {
+      editable,
+      extensions: [
+        StarterKit,
+        Link.configure({
+          linkOnPaste: false,
+          protocols: [
+            {
+              scheme: "tel",
+              optionalSlashes: true,
+            },
+          ],
+        }),
+        Underline,
+      ],
+      editorProps: {
+        attributes: {
+          class:
+            "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2",
+        },
       },
+      // content: `
+      //     <h2>
+      //       Hi there,
+      //     </h2>
+      //     <p>
+      //       this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+      //     </p>
+      //     <ul>
+      //       <li>
+      //         That’s a bullet list with one …
+      //       </li>
+      //       <li>
+      //         … or two list items.
+      //       </li>
+      //     </ul>
+      //     <p>
+      //       Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+      //     </p>
+      // <pre><code class="language-css">body {
+      //   display: none;
+      // }</code></pre>
+      //     <p>
+      //       I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+      //     </p>
+      //     <blockquote>
+      //       Wow, that’s amazing. Good work, boy! 👏
+      //       <br />
+      //       — Mom
+      //     </blockquote>
+      //   `,
+      content: "hey",
     },
-    // content: `
-    //     <h2>
-    //       Hi there,
-    //     </h2>
-    //     <p>
-    //       this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-    //     </p>
-    //     <ul>
-    //       <li>
-    //         That’s a bullet list with one …
-    //       </li>
-    //       <li>
-    //         … or two list items.
-    //       </li>
-    //     </ul>
-    //     <p>
-    //       Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-    //     </p>
-    // <pre><code class="language-css">body {
-    //   display: none;
-    // }</code></pre>
-    //     <p>
-    //       I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-    //     </p>
-    //     <blockquote>
-    //       Wow, that’s amazing. Good work, boy! 👏
-    //       <br />
-    //       — Mom
-    //     </blockquote>
-    //   `,
-    content: "hey",
-  });
+    [editable]
+  );
 
   if (!editor) return null;
   return (
