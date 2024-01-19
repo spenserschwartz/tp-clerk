1d9e9a5397280e87aa70699066e6297441e1478f
Spenser Schwartz <spensernet@gmail.com>
2024-01-19 15:03:27 -0800

    EditorBubbleMenu

--- src/components/tiptap/components/BubbleMenu.tsx
+++ src/components/tiptap/components/BubbleMenu.tsx
@@ -1,32 +1,158 @@
-import { BubbleMenu, Editor, EditorContent, useEditor } from "@tiptap/react";
+import { BubbleMenu, type Editor } from "@tiptap/react";
+import { useCallback, useState } from "react";
+
+import {
+  TextBoldIcon,
+  TextBulletListIcon,
+  TextItalicIcon,
+  TextLinkIcon,
+  TextOrderedListIcon,
+  TextUnderlineIcon,
+} from "public/icons";
+import { TextLinkModal } from "~/components/modal";
 
 interface BubbleMenuProps {
   editor: Editor;
 }
 
 const EditorBubbleMenu = ({ editor }: BubbleMenuProps) => {
+  const buttonClassName = (node: string) => {
+    return `rounded p-1 hover:bg-gray-600 ${
+      editor.isActive(node) ? "bg-blue-500" : ""
+    }`;
+  };
+  const [openTextLinkModal, setOpenTextLinkModal] = useState(false);
+
+  const toggleBold = useCallback(() => {
+    editor?.chain().focus().toggleBold().run();
+  }, [editor]);
+
+  const toggleItalic = useCallback(() => {
+    editor?.chain().focus().toggleItalic().run();
+  }, [editor]);
+
+  const toggleUnderline = useCallback(() => {
+    editor?.chain().focus().toggleUnderline().run();
+  }, [editor]);
+
+  const toggleLink = useCallback(() => {
+    if (!openTextLinkModal) {
+      setOpenTextLinkModal(true);
+    } else {
+      editor?.chain().focus().unsetLink().run();
+    }
+  }, [editor, openTextLinkModal]);
+
+  const toggleOrderedList = useCallback(() => {
+    editor?.chain().focus().toggleOrderedList().run();
+  }, [editor]);
+
+  const toggleBulletList = useCallback(() => {
+    editor?.chain().focus().toggleBulletList().run();
+  }, [editor]);
+
   return (
     editor && (
-      <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive("bold") ? "is-active" : ""}
+      <div>
+        <BubbleMenu
+          editor={editor}
+          tippyOptions={{ duration: 100 }}
+          className="flex space-x-1 rounded-lg bg-gray-800 p-1 text-white"
         >
-          bold
+          {/* <button
+          className={classNames("menu-button", {
+            "is-active": editor.isActive("link")
+          })}
+          onClick={openModal}
+        >
+          <Icons.Link />
+        </button> */}
+          {/* Bold */}
+          <button
+            className={buttonClassName("bold")}
+            type="button"
+            onClick={toggleBold}
+          >
+            <TextBoldIcon />
+          </button>
+
+          {/* Italic */}
+          <button
+            className={buttonClassName("italic")}
+            type="button"
+            onClick={toggleItalic}
+          >
+            <TextItalicIcon />
+          </button>
+
+          {/* Underline */}
+          <button
+            className={buttonClassName("underline")}
+            type="button"
+            onClick={toggleUnderline}
+          >
+            <TextUnderlineIcon />
+          </button>
+
+          {/* Link */}
+          <button
+            className={buttonClassName("link")}
+            type="button"
+            onClick={toggleLink}
+          >
+            <TextLinkIcon />
+          </button>
+
+          {/* Ordered List (Numbered) */}
+          <button
+            className={buttonClassName("orderedList")}
+            type="button"
+            onClick={toggleOrderedList}
+          >
+            <TextOrderedListIcon />
+          </button>
+
+          {/* Bullet List */}
+          <button
+            className={buttonClassName("bulletList")}
+            type="button"
+            onClick={toggleBulletList}
+          >
+            <TextBulletListIcon />
+          </button>
+          {/* <button
+          className={classNames("menu-button", {
+            "is-active": editor.isActive("underline"),
+          })}
+          onClick={toggleUnderline}
+        >
+          <Icons.Underline />
+        </button>
+       
+        <button
+          className={classNames("menu-button", {
+            "is-active": editor.isActive("strike"),
+          })}
+          onClick={toggleStrike}
+        >
+          <Icons.Strikethrough />
         </button>
         <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive("italic") ? "is-active" : ""}
+          className={classNames("menu-button", {
+            "is-active": editor.isActive("code"),
+          })}
+          onClick={toggleCode}
         >
-          italic
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive("strike") ? "is-active" : ""}
-        >
-          strike
-        </button>
-      </BubbleMenu>
+          <Icons.Code />
+        </button> */}
+        </BubbleMenu>
+
+        <TextLinkModal
+          openModal={openTextLinkModal}
+          setOpenModal={setOpenTextLinkModal}
+          editor={editor}
+        />
+      </div>
     )
   );
 };
