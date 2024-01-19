c6b8c64a2c07da40421efceb1d48c377069ee4e1
Spenser Schwartz <spensernet@gmail.com>
2024-01-19 13:38:37 -0800

    Editor update

--- src/components/tiptap/Editor.tsx
+++ src/components/tiptap/Editor.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "@clerk/nextjs";
 import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
 import Underline from "@tiptap/extension-underline";
@@ -14,9 +15,11 @@ interface TextEditorProps {
 }
 
 const TextEditor = ({ data }: TextEditorProps) => {
+  const { user } = useUser();
   const { editItineraryUserNotes } = useEditItineraryUserNotes();
   const { userNotes, id } = data;
   const [currentNotes, setCurrentNotes] = useState<string>(userNotes ?? "");
+  const userCanEdit = user?.id === data.userId;
 
   const handleBlur = () => {
     if (!editor) return;
@@ -27,35 +30,38 @@ const TextEditor = ({ data }: TextEditorProps) => {
     }
   };
 
-  const editor = useEditor({
-    // content,
-    content: "",
-    editorProps: {
-      attributes: {
-        class:
-          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2",
+  const editor = useEditor(
+    {
+      content: data.userNotes,
+      editable: userCanEdit,
+      editorProps: {
+        attributes: {
+          class:
+            "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2",
+        },
       },
+      extensions: [
+        StarterKit,
+        Link.configure({
+          linkOnPaste: false,
+          protocols: [
+            {
+              scheme: "tel",
+              optionalSlashes: true,
+            },
+          ],
+        }),
+        Placeholder.configure({
+          placeholder: "Add notes here...",
+          emptyEditorClass:
+            "cursor-text before:opacity-50 before:content-[attr(data-placeholder)] before:absolute before:left-2 before:text-mauve-11 before-pointer-events-none before:flex before:items-center",
+        }),
+        Underline,
+      ],
+      onBlur: handleBlur,
     },
-    extensions: [
-      StarterKit,
-      Link.configure({
-        linkOnPaste: false,
-        protocols: [
-          {
-            scheme: "tel",
-            optionalSlashes: true,
-          },
-        ],
-      }),
-      Placeholder.configure({
-        placeholder: "Add notes here...",
-        emptyEditorClass:
-          "cursor-text before:opacity-50 before:content-[attr(data-placeholder)] before:absolute before:left-2 before:text-mauve-11 before-pointer-events-none before:flex before:items-center",
-      }),
-      Underline,
-    ],
-    onBlur: handleBlur,
-  });
+    [userCanEdit] // Only reinitialize editor if userCanEdit changes
+  );
 
   // Update userNotes if editor content has changed (debounced)
   const [debouncedEditor] = useDebounce(editor?.state.doc.content, 1000);
@@ -68,7 +74,7 @@ const TextEditor = ({ data }: TextEditorProps) => {
     }
   }, [currentNotes, debouncedEditor, editItineraryUserNotes, editor, id]);
 
-  if (!editor) return null;
+  if (!editor || (!userCanEdit && currentNotes === "")) return null;
   return (
     <div>
       <p className="w-full truncate rounded-full border-0 p-0 text-center text-xl font-extrabold outline-none transition duration-150 ease-in-out md:px-8 md:text-3xl lg:text-4xl">
