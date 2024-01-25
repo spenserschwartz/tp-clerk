f9bef8e2c02094d37ff7682effc8d31ecb3125bf
Spenser Schwartz <spensernet@gmail.com>
2024-01-25 12:42:04 -0800

    update Header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -68,38 +68,35 @@ export default function Header() {
           </nav>
 
           <div className="hidden md:flex md:flex-1 md:justify-end">
-            {/* Header Sign In Links */}
             <SignedIn>
               <UserButton />
             </SignedIn>
             <SignedOut>
-              <li>
-                <Link
-                  href="/sign-in"
-                  className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
-                >
-                  Sign in
-                </Link>
-              </li>
+              {/* Header Sign In Links */}
+              <Link
+                href="/sign-in"
+                className="flex items-center px-5 py-3 font-medium text-gray-600 transition duration-150 ease-in-out hover:text-gray-900"
+              >
+                Sign in
+              </Link>
+
               {/* Header Sign Up */}
-              <li>
-                <Link
-                  href="/sign-in"
-                  className="btn-sm ml-3 bg-gray-900 text-gray-200 hover:bg-gray-800"
+              <Link
+                href="/sign-in"
+                className="btn-sm ml-3 bg-gray-900 text-gray-200 hover:bg-gray-800"
+              >
+                <span>Sign up</span>
+                <svg
+                  className="-mr-1 ml-2 h-3 w-3 shrink-0 fill-current text-gray-400"
+                  viewBox="0 0 12 12"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <span>Sign up</span>
-                  <svg
-                    className="-mr-1 ml-2 h-3 w-3 shrink-0 fill-current text-gray-400"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </Link>
-              </li>
+                  <path
+                    d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </Link>
             </SignedOut>
           </div>
 
