2cb546d3db5d2ff7360b786330ce292ff9d334cc
Spenser Schwartz <spensernet@gmail.com>
2024-01-25 14:16:00 -0800

    HeaderLogo forwardref

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
-import { forwardRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { HeaderLogo, Logo, MobileMenu } from "./index";
 import { headerNavigation } from "./utils";
@@ -13,32 +13,6 @@ export default function Header() {
   const scrollHandler = () => {
     window.scrollY > 10 ? setTop(false) : setTop(true);
   };
-
-  // interface HeaderLogoProps {
-  //   onClick?: () => void;
-  //   href?: string;
-  // }
-
-  // const HeaderLogo = forwardRef<HTMLDivElement, HeaderLogoProps>(
-  //   ({ onClick }, ref) => {
-  //     return (
-  //       <div
-  //         onClick={onClick}
-  //         ref={ref}
-  //         className="flex flex-1 cursor-pointer items-center align-middle md:justify-start"
-  //       >
-  //         <Logo />
-  //         <h1 className="leading-tigher flex items-center text-3xl font-extrabold tracking-tighter md:text-4xl">
-  //           Travel
-  //           <span className="bg-gradient-to-r from-purple-500 to-violet-400 bg-clip-text text-transparent">
-  //             Perfect
-  //           </span>
-  //         </h1>
-  //       </div>
-  //     );
-  //   }
-  // );
-  // HeaderLogo.displayName = "HeaderLogo";
 
   useEffect(() => {
     scrollHandler();
