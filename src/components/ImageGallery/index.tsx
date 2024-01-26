59596f37b41d2438a1ddc09cc794a4a59c77a2f2
Spenser Schwartz <spensernet@gmail.com>
2024-01-26 15:00:49 -0800

    flex-grow

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -4,11 +4,9 @@ interface ImageGalleryProps {
   images: string[];
 }
 
-const ImageGallery = ({
-  images = ["a", "b", "c", "d", "e"],
-}: ImageGalleryProps) => {
+const ImageGallery = ({ images }: ImageGalleryProps) => {
   return (
-    <div className="flex min-w-[400px] max-w-4xl justify-center border border-red-400">
+    <div className="min-w-[400px] max-w-4xl flex-grow justify-center ">
       <div className="grid h-full w-full grid-cols-4 grid-rows-2 gap-4 border border-green-400">
         <div className="aspect-h-1 aspect-w-1 relative col-span-2 row-span-2 bg-gray-500">
           {/* Placeholder for large image */}
