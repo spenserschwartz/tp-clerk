b6fb736dd861194d5b4598d935c13ee034dc9b1c
Spenser Schwartz <spensernet@gmail.com>
2024-01-30 15:12:25 -0800

    DeleteUpvote Success

--- src/components/profiles/place/components/title.tsx
+++ src/components/profiles/place/components/title.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState, type MouseEvent } from "react";
 import { LoginModal } from "~/components/modal";
 import { type AttractionByNameType } from "~/types/router";
 import { api } from "~/utils/api";
+import { useDeleteUpvoteFromUser } from "~/utils/hooks";
 
 interface PlaceTitleProps {
   data: AttractionByNameType;
@@ -12,25 +13,31 @@ interface PlaceTitleProps {
 const PlaceTitle = ({ data }: PlaceTitleProps) => {
   const { user, isSignedIn, isLoaded } = useUser();
   const { name, id } = data ?? {};
-
-  const [hasUserUpvoted, setHasUserUpvoted] = useState(false);
+  const { deleteUpvoteFromUser, isDeletingUpvote, upvoteDeleted } =
+    useDeleteUpvoteFromUser();
+  const [userUpvoted, setUserUpvoted] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const { data: placeData } = api.upvotes.getByUserAndId.useQuery({
     attractionId: id ?? "",
     userId: user ? user.id : "",
   });
 
-  console.log("userHasUpvoted", hasUserUpvoted);
+  console.log("userHasUpvoted", userUpvoted);
 
   // Check if user has upvoted this place from the server
   useEffect(() => {
-    if (placeData) setHasUserUpvoted(true);
+    if (placeData) setUserUpvoted(true);
   }, [placeData]);
 
   const handleUpvote = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (!isSignedIn) return setOpenModal(true);
     else {
+      if (userUpvoted) {
+        setUserUpvoted(false);
+        // void api.upvotes.delete.mutation({ attractionId: id ?? "" });
+        deleteUpvoteFromUser({ attractionId: id ?? "" });
+      }
     }
   };
 
@@ -43,7 +50,7 @@ const PlaceTitle = ({ data }: PlaceTitleProps) => {
       </div>
       <div className="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
         <button onClick={handleUpvote}>
-          <HeartIcon enabled={hasUserUpvoted} />
+          <HeartIcon enabled={userUpvoted} />
         </button>
       </div>
 
