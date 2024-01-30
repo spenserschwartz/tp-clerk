50d25a375a55780a6f12950e314d32bca6af930c
Spenser Schwartz <spensernet@gmail.com>
2024-01-30 15:05:57 -0800

    useDeleteUpvoteFromUser

---
+++ src/utils/hooks/useDeleteUpvoteFromUser.ts
@@ -0,0 +1,30 @@
+import { toast } from "react-hot-toast";
+import { api } from "~/utils/api";
+
+const useDeleteUpvoteFromUser = () => {
+  const {
+    mutate: deleteUpvoteFromUser,
+    isLoading: isDeletingUpvote,
+    isSuccess: upvoteDeleted,
+  } = api.upvotes.delete.useMutation({
+    onSuccess: () => {
+      console.log("DeleteUpvote Success");
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage?.[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to delete itinerary! Please try again later.");
+      }
+    },
+  });
+
+  return {
+    deleteUpvoteFromUser,
+    isDeletingUpvote,
+    upvoteDeleted,
+  };
+};
+
+export default useDeleteUpvoteFromUser;
