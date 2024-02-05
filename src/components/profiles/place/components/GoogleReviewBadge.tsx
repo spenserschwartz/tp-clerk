02549f68899d60025ed41e6ec2df2674a0b6595f
Spenser Schwartz <spensernet@gmail.com>
2024-02-05 07:50:22 -0800

    <a> to open google url in new tab

--- src/components/profiles/place/components/GoogleReviewBadge.tsx
+++ src/components/profiles/place/components/GoogleReviewBadge.tsx
@@ -14,20 +14,22 @@ const GoogleReviewBadge = ({ googleData }: GoogleReviewBadgeProps) => {
 
   return (
     <div className="w-60 overflow-hidden rounded-lg border-2 border-gray-200 bg-white p-6 text-center shadow-lg">
-      <div className="mb-4 flex justify-center">
-        <GoogleIcon />
-      </div>
-      <p className="mb-2  text-lg text-gray-700">Google Reviews</p>
-      <h2 className="mb-2 text-4xl font-bold text-gray-900">
-        {rating ?? null}
-      </h2>
-      <p className="mb-4 text-sm text-gray-600">
-        {`Based on ${user_ratings_total?.toLocaleString("en-us")} reviews` ??
-          null}
-      </p>
-      <div className="flex justify-center">
-        <StarRatings value={rating ?? 0} />
-      </div>
+      <a href={googleData.url} target="_blank">
+        <div className="mb-4 flex justify-center">
+          <GoogleIcon />
+        </div>
+        <p className="mb-2  text-lg text-gray-700">Google Reviews</p>
+        <h2 className="mb-2 text-4xl font-bold text-gray-900">
+          {rating ?? null}
+        </h2>
+        <p className="mb-4 text-sm text-gray-600">
+          {`Based on ${user_ratings_total?.toLocaleString("en-us")} reviews` ??
+            null}
+        </p>
+        <div className="flex justify-center">
+          <StarRatings value={rating ?? 0} />
+        </div>
+      </a>
     </div>
   );
 };
