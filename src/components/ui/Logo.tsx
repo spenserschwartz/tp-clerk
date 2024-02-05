bb17e3861cfaccd71b603612953af1b8e2b6b1db
Spenser Schwartz <spensernet@gmail.com>
2024-02-05 15:17:01 -0800

    Image height and width ratio fix

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href="/" className="block" aria-label="TravelPerfect">
-      <Image
-        src="/images/penguin_purp_cropped.png"
-        width={32}
-        height={50}
-        alt="HeaderIcon"
-      />
+    <Link href="/" aria-label="TravelPerfect">
+      <div className="w-8">
+        {/* Tailwind class for width */}
+        <Image
+          src="/images/penguin_purp_cropped.png"
+          width={328} // Original image width
+          height={516} // Original image height
+          alt="HeaderIcon"
+        />
+      </div>
     </Link>
   );
 }
