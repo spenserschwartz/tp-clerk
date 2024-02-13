e74749e9e8c520fe6d7493af347d4ae596c39c15
Spenser Schwartz <spensernet@gmail.com>
2024-02-13 15:41:19 -0800

    splash

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -12,13 +12,13 @@ export default SplashPage;
 // ChIJN1t_tDeuEmsRUsoyG83frY4   Google Office Sydney
 // ChIJj61dQgK6j4AR4GeTYWZsKWw   from example on
 
-// curl -L -X GET 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Big%20Ben%20%20London&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cuser_ratings_total&key=AIzaSyBFPo3i-80eWrthWehZU-nBaKRQ-F11R58'
+// curl -L -X GET 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Big%20Ben%20%20London&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cuser_ratings_total&key=MY_API_KEY_HERE'
 
-// curl -L -X GET 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=The%20Shard%20%20London&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Cuser_ratings_total&key=AIzaSyBFPo3i-80eWrthWehZU-nBaKRQ-F11R58'
+// curl -L -X GET 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=The%20Shard%20%20London&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Cuser_ratings_total&key=MY_API_KEY_HERE'
 
 // curl -X POST -d '{
 //   "textQuery" : "Charles Dickens Museum London"
 // }' \
-// -H 'Content-Type: application/json' -H 'X-Goog-Api-Key: AIzaSyBFPo3i-80eWrthWehZU-nBaKRQ-F11R58' \
+// -H 'Content-Type: application/json' -H 'X-Goog-Api-Key: MY_API_KEY_HERE' \
 // -H 'X-Goog-FieldMask: *' \
 // 'https://places.googleapis.com/v1/places:searchText'
