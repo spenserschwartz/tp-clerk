0829420aa98a0e70d7dfb7047dd95469bee5a38b
Spenser Schwartz <spensernet@gmail.com>
2024-02-16 12:03:10 -0800

    vis.gl/react-google-maps replaces googlemaps/react-wrapper

--- src/components/ItineraryImageGrid/index.tsx
+++ src/components/ItineraryImageGrid/index.tsx
@@ -1,7 +1,8 @@
-import { Wrapper as GoogleMapsWrapper } from "@googlemaps/react-wrapper";
+// import { Wrapper as GoogleMapsWrapper } from "@googlemaps/react-wrapper";
+import { APIProvider as GoogleAPIProvider } from "@vis.gl/react-google-maps";
 
 import { type ItineraryWithCityInfoType } from "~/types/router";
-import { googleMapsRender } from "~/utils/google";
+import { LoadingPage } from "../loading";
 import ItineraryGridElement from "./components/GridElement";
 
 interface ItineraryImageGridProps {
@@ -9,12 +10,11 @@ interface ItineraryImageGridProps {
 }
 
 const ItineraryImageGrid = ({ itineraries }: ItineraryImageGridProps) => {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+  if (!apiKey) return <LoadingPage />;
   return (
-    <GoogleMapsWrapper
-      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ""}
-      render={googleMapsRender}
-      libraries={["places"]}
-    >
+    <GoogleAPIProvider apiKey={apiKey}>
       <ul
         role="list"
         className="mt-4 grid grid-cols-1 place-items-center gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:gap-8 2xl:grid-cols-5 "
@@ -25,7 +25,7 @@ const ItineraryImageGrid = ({ itineraries }: ItineraryImageGridProps) => {
           </div>
         ))}
       </ul>
-    </GoogleMapsWrapper>
+    </GoogleAPIProvider>
   );
 };
 
