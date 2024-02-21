e1c86b2c551f44fe06a8f1b04defa132084e7465
Spenser Schwartz <spensernet@gmail.com>
2024-02-20 23:10:12 -0800

    custom PlaceNew interface

--- src/types/google.ts
+++ src/types/google.ts
@@ -7,139 +7,9 @@ import PlacePhoto = google.maps.places.PlacePhoto;
 import PlaceResult = google.maps.places.PlaceResult;
 import PlacesService = google.maps.places.PlacesService;
 
-/*
-  Custom interface made for Place Details (New) response
-  https://developers.google.com/maps/documentation/places/web-service/place-details#required-parameters
-
-  This is the type of response that we get from the Google Place Details API
-  https://developers.google.com/maps/documentation/places/web-service/reference/rest/v1/places
-*/
-interface PlaceDetailsNewResponse {
-  accessibilityOptions?: AccessibilityOptions;
-  addressComponents?: AddressComponent[];
-  adrFormatAddress?: string;
-  businessStatus?: string;
-  currentOpeningHours?: CurrentOpeningHours;
-  displayName?: DisplayName;
-  editorialSummary?: EditorialSummary;
-  formattedAddress?: string;
-  googleMapsUri?: string;
-  iconBackgroundColor?: string;
-  iconMaskBaseUri?: string;
-  id?: string;
-  internationalPhoneNumber?: string;
-  location?: Location;
-  name?: string;
-  nationalPhoneNumber?: string;
-  photos?: Photo[];
-  plusCode?: PlusCode;
-  rating?: number;
-  regularOpeningHours?: RegularOpeningHours;
-  reviews?: Review[];
-  shortFormattedAddress?: string;
-  types?: string[];
-  userRatingCount?: number;
-  utcOffsetMinutes?: number;
-  viewport?: Viewport;
-  websiteUri?: string;
+interface PlaceNew extends Place {
+  websiteUri: string;
 }
-
-interface AddressComponent {
-  languageCode?: string;
-  longText?: string;
-  shortText?: string;
-  types?: string[];
-}
-
-interface AuthorAttribution {
-  displayName?: string;
-  photoUri?: string;
-  uri?: string;
-}
-
-interface CurrentOpeningHours {
-  openNow?: boolean;
-  periods?: Period[];
-  weekdayDescriptions?: string[];
-}
-
-interface DateDetail {
-  day?: number;
-  month?: number;
-  year?: number;
-}
-
-interface DisplayName {
-  languageCode?: string;
-  text?: string;
-}
-
-interface EditorialSummary {
-  languageCode?: string;
-  text?: string;
-}
-
-interface Location {
-  latitude?: number;
-  longitude?: number;
-}
-
-interface OpeningDetail {
-  date?: DateDetail;
-  day?: number;
-  hour?: number;
-  minute?: number;
-}
-
-interface Period {
-  close?: OpeningDetail;
-  open?: OpeningDetail;
-}
-
-interface Photo {
-  authorAttributions?: AuthorAttribution[];
-  heightPx?: number;
-  name?: string;
-  widthPx?: number;
-}
-
-interface PlusCode {
-  compoundCode?: string;
-  globalCode?: string;
-}
-
-interface RegularOpeningHours {
-  openNow?: boolean;
-  periods?: Period[];
-  weekdayDescriptions?: string[];
-}
-
-interface Review {
-  authorAttribution?: AuthorAttribution;
-  name?: string;
-  originalText?: Text;
-  publishTime?: string;
-  rating?: number;
-  relativePublishTimeDescription?: string;
-  text?: Text;
-}
-
-interface Text {
-  languageCode?: string;
-  text?: string;
-}
-
-interface Viewport {
-  high?: Location;
-  low?: Location;
-}
-
-interface AccessibilityOptions {
-  wheelchairAccessibleEntrance?: boolean;
-  wheelchairAccessibleParking?: boolean;
-}
-
-// ***
 
 interface PlaceResultWithLatLng extends Omit<PlaceResult, "geometry"> {
   geometry: {
@@ -162,7 +32,7 @@ export type {
   AutocompleteRequest,
   AutocompleteSessionToken,
   Place,
-  PlaceDetailsNewResponse,
+  PlaceNew,
   PlacePhoto,
   PlaceResult,
   PlaceResultWithLatLng,
