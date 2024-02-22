4e0f3a55feef317d1d9f1f3189e98b9437b21c29
Spenser Schwartz <spensernet@gmail.com>
2024-02-22 08:07:56 -0800

    SocialLinks

--- src/pages/spenser/index.tsx
+++ src/pages/spenser/index.tsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
+import Link from "next/link";
 import { type ReactElement } from "react";
 import { PortfolioLayout } from "~/components/layout";
 import { Container } from "~/components/portfolio/Container";
+import { GitHubIcon, LinkedInIcon } from "~/components/portfolio/SocialIcons";
 import { type NextPageWithLayout } from "~/types/pages";
+
+function SocialLink({
+  icon: Icon,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Link> & {
+  icon: React.ComponentType<{ className?: string }>;
+}) {
+  return (
+    <Link className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    </Link>
+  );
+}
 
 const PortfolioPage: NextPageWithLayout = () => {
   return (
@@ -19,6 +34,18 @@ const PortfolioPage: NextPageWithLayout = () => {
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             {`I'm Spenser, a software developer based in Los Angeles. I'm the found of TravelPerfect, a travel app that helps you plan your perfect trip. I'm also a full-stack developer with a passion for building beautiful, functional, and user-friendly web applications.`}
           </p>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://www.github.com/spenserschwartz"
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+            />
+            <SocialLink
+              href="https://www.linkedin.com/in/spenser-schwartz/"
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
+            />
+          </div>
         </div>
       </Container>
     </div>
