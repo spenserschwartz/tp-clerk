24d16bd480a4a121c09da24ae320101a01073915
Spenser Schwartz <spensernet@gmail.com>
2024-02-21 20:34:47 -0800

    PortfolioLayout

---
+++ src/components/layout/Portfolio.tsx
@@ -0,0 +1,37 @@
+import { type PropsWithChildren } from "react";
+import { Header } from "../portfolio/Header";
+import { ThemeProvider } from "../portfolio/ThemeProvider";
+
+const PortfolioLayout = (props: PropsWithChildren) => {
+  return (
+    // <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="flex h-full min-h-screen bg-zinc-50 dark:bg-black">
+        {/* <Providers> */}
+        <div className="flex w-full">
+          {/* Inner Layout */}
+          <div className="fixed inset-0 flex justify-center sm:px-8">
+            <div className="flex w-full max-w-7xl lg:px-8">
+              <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+            </div>
+          </div>
+          <div className="relative flex w-full flex-col">
+            <Header />
+            <main className="flex-auto">{props.children}</main>
+            {/* <Footer /> */}
+          </div>
+        </div>
+        {/* </Providers> */}
+      </div>
+    </ThemeProvider>
+
+    // </html>
+  );
+};
+
+export default PortfolioLayout;
