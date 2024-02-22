fc583d69fbcc59d662ac5a7e1fdc966491f6f988
Spenser Schwartz <spensernet@gmail.com>
2024-02-22 06:56:12 -0800

    Add Head, update text

--- src/pages/spenser/index.tsx
+++ src/pages/spenser/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { type ReactElement } from "react";
 import { PortfolioLayout } from "~/components/layout";
 import { Container } from "~/components/portfolio/Container";
@@ -6,16 +7,17 @@ import { type NextPageWithLayout } from "~/types/pages";
 const PortfolioPage: NextPageWithLayout = () => {
   return (
     <div>
+      <Head>
+        <title>Spenser Schwartz</title>
+      </Head>
+
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software designer, founder, and travel enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            {`I'm Spenser, a software developer based in Los Angeles. I'm the found of TravelPerfect, a travel app that helps you plan your perfect trip. I'm also a full-stack developer with a passion for building beautiful, functional, and user-friendly web applications.`}
           </p>
         </div>
       </Container>
