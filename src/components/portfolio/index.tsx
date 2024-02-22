179acc28a62927d8d74935e06ee81fa716f1b848
Spenser Schwartz <spensernet@gmail.com>
2024-02-22 14:44:20 -0800

    barrel import

---
+++ src/components/portfolio/index.tsx
@@ -0,0 +1,16 @@
+import { Container, ContainerInner, ContainerOuter } from "./Container";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { GitHubIcon, LinkedInIcon } from "./SocialIcons";
+import { ThemeProvider } from "./ThemeProvider";
+
+export {
+  Container,
+  ContainerInner,
+  ContainerOuter,
+  Footer,
+  GitHubIcon,
+  Header,
+  LinkedInIcon,
+  ThemeProvider,
+};
