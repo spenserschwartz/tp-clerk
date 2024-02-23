af34164323ba99a424d3c2a391942e0f9b3936b5
Spenser Schwartz <spensernet@gmail.com>
2024-02-23 14:52:50 -0800

    barrel import

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,6 +2,14 @@ import GitHubIcon from "./GitHub";
 import LinkedInIcon from "./LinkedIn";
 import NextIcon from "./Next";
 import NodeIcon from "./Node";
+import ReactIcon from "./React";
 import TypeScriptIcon from "./TypeScript";
 
-export { GitHubIcon, LinkedInIcon, NextIcon, NodeIcon, TypeScriptIcon };
+export {
+  GitHubIcon,
+  LinkedInIcon,
+  NextIcon,
+  NodeIcon,
+  ReactIcon,
+  TypeScriptIcon,
+};
