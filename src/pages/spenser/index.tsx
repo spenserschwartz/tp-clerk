1f07282b72ea14bff60faf119c9a9b472a96ffe9
Spenser Schwartz <spensernet@gmail.com>
2024-02-23 15:59:18 -0800

    purple text

--- src/pages/spenser/index.tsx
+++ src/pages/spenser/index.tsx
@@ -25,7 +25,7 @@ const PortfolioPage: NextPageWithLayout = () => {
 
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-purple-400 sm:text-5xl">
             Software designer, founder, and travel enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
