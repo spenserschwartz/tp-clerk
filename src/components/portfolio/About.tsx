23ccc571e1174360030745fa8f940aeb94175136
Spenser Schwartz <spensernet@gmail.com>
2024-02-23 13:54:56 -0800

    update text

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -1,36 +1,26 @@
 import React from "react";
+import NextIcon from "~/icons/Next";
+import NodeIcon from "~/icons/Node";
 
 const About = () => {
   return (
     <div className="lg:order-first lg:row-span-2">
       <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
         <p>
-          I’ve loved making things for as long as I can remember, and wrote my
-          first program when I was 6 years old, just two weeks after my mom
-          brought home the brand new Macintosh LC 550 that I taught myself to
-          type on.
+          I’ve loved making things for as long as I can remember, and I&apos;ve
+          always thought of how to solve real world solutions with technology. I
+          was drawn to the web at a young age, and I&apos;ve been building
+          applications ever since.
         </p>
         <p>
-          The only thing I loved more than computers as a kid was space. When I
-          was 8, I climbed the 40-foot oak tree at the back of our yard while
-          wearing my older sister’s motorcycle helmet, counted down from three,
-          and jumped — hoping the tree was tall enough that with just a bit of
-          momentum I’d be able to get to orbit.
-        </p>
-        <p>
-          I spent the next few summers indoors working on a rocket design, while
-          I recovered from the multiple surgeries it took to fix my badly broken
-          legs. It took nine iterations, but when I was 15 I sent my dad’s
-          Blackberry into orbit and was able to transmit a photo back down to
-          our family computer from space.
-        </p>
-        <p>
-          Today, I’m the founder of Planetaria, where we’re working on civilian
-          space suits and manned shuttle kits you can assemble at home so that
-          the next generation of kids really <em>can</em> make it to orbit —
-          from the comfort of their own backyards.
+          In my free time, I enjoy traveling and have developed an application
+          to help others plan their perfect trip or vacation using artificial
+          intelligence and machine learning. Please feel free to reach out to me
+          if you have any questions or would like to collaborate!
         </p>
       </div>
+      <NextIcon />
+      <NodeIcon />
     </div>
   );
 };
