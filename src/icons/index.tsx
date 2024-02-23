8183048f8b4339d393d677e23a92e5a6e3ce21b8
Spenser Schwartz <spensernet@gmail.com>
2024-02-23 15:57:13 -0800

    TailwindIcon

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,15 +1,19 @@
 import GitHubIcon from "./GitHub";
+import GraphQLIcon from "./GraphQL";
 import LinkedInIcon from "./LinkedIn";
 import NextIcon from "./Next";
 import NodeIcon from "./Node";
 import ReactIcon from "./React";
+import TailwindIcon from "./Tailwind";
 import TypeScriptIcon from "./TypeScript";
 
 export {
   GitHubIcon,
+  GraphQLIcon,
   LinkedInIcon,
   NextIcon,
   NodeIcon,
   ReactIcon,
+  TailwindIcon,
   TypeScriptIcon,
 };
