4e0ae9ff04730a810806c59b1e57f03589a9109f
Spenser Schwartz <spensernet@gmail.com>
2024-02-22 20:15:38 -0800

    update text

--- src/pages/spenser/index.tsx
+++ src/pages/spenser/index.tsx
@@ -32,7 +32,7 @@ const PortfolioPage: NextPageWithLayout = () => {
             Software designer, founder, and travel enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            {`I'm Spenser, a software developer based in Los Angeles. I'm the found of TravelPerfect, a travel app that helps you plan your perfect trip. I'm also a full-stack developer with a passion for building beautiful, functional, and user-friendly web applications.`}
+            {`I'm Spenser, a software developer based in Los Angeles. I'm the creator of TravelPerfect, a travel app that helps you plan your perfect trip. I'm also a full-stack developer with a passion for building beautiful, functional, and user-friendly web applications.`}
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
