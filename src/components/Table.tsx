4b660fca2231f6e496d310cc8b4a126e4d46ff49
Spenser Schwartz <spensernet@gmail.com>
2024-03-08 11:27:04 -0800

    Table column widths

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -34,39 +34,20 @@ export default function Table({ places }: TableProps) {
                     </th>
                     <th
                       scope="col"
-                      className="w-1/2 border border-green-500 px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      className="w-2/3 border border-green-500 px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Description
                     </th>
 
                     <th
                       scope="col"
-                      className="relative w-1/4 border border-blue-400 py-3.5 pl-3 pr-4 sm:pr-6"
+                      className="relative w-1/12 border border-blue-400 py-3.5 pl-3 pr-4 sm:pr-6"
                     >
                       <span className="sr-only">Edit</span>
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                    <tr key={person.email}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                        {person.name}
-                      </td>
-
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.role}
-                      </td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
                   {places.map((place) => (
                     <tr key={place.id}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
