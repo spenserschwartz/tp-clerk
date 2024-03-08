c639e6baf22b50bdc142f084b8351938fb26a983
Spenser Schwartz <spensernet@gmail.com>
2024-03-08 15:39:14 -0800

    w-full on div to fit contents, replace min-w-full

--- src/components/layout/Root.tsx
+++ src/components/layout/Root.tsx
@@ -9,14 +9,14 @@ export const RootLayout = (props: PropsWithChildren) => {
       <Header />
       <div className="flex min-h-screen flex-col items-center overflow-hidden supports-[overflow:clip]:overflow-clip">
         {/* Padding is also in header component, localized because of fixed position */}
-        <div className="min-w-full px-2 md:px-10 lg:px-20">
+        <div className="w-full px-2 md:px-10 lg:px-20">
           {/* Spacing between header and main content */}
           <div className="mb-24 md:mb-20" />
 
           <Breadcrumbs />
 
           {/* Main Content */}
-          <main className="">{props.children}</main>
+          <main className="border border-red-400">{props.children}</main>
         </div>
 
         {/* Grow the page so that footer is at bottom of page if there is no scroll */}
