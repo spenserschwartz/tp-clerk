16fb4fd97dcdbff605e0269f9461d7b2d22e8b5e
Spenser Schwartz <spensernet@gmail.com>
2024-03-12 14:34:02 -0500

    remove unused code

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -52,42 +52,6 @@ export default function Table({ cityId, places }: TableProps) {
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
                 {places.map((place) => (
-                  // <tr key={place.id}>
-                  //   {/* Adjusted cells for ellipsis */}
-                  //   <td
-                  //     className="w-1/3 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
-                  //     style={{
-                  //       minWidth: "100px",
-                  //       maxWidth: "200px",
-                  //       overflow: "hidden",
-                  //       textOverflow: "ellipsis",
-                  //       whiteSpace: "nowrap",
-                  //     }}
-                  //   >
-                  //     {place.displayName?.text}
-                  //   </td>
-
-                  //   <td
-                  //     className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell"
-                  //     style={{
-                  //       minWidth: "100px",
-                  //       maxWidth: "300px",
-                  //       overflow: "hidden",
-                  //       textOverflow: "ellipsis",
-                  //       whiteSpace: "nowrap",
-                  //     }}
-                  //   >
-                  //     {place.editorialSummary?.text}
-                  //   </td>
-                  //   <td className="relative py-4 text-right text-sm font-medium ">
-                  //     <div className="flex items-center justify-center">
-                  //       <HeartIcon enabled={false} />
-                  //       <span className="sr-only">
-                  //         Like Button for {place.displayName?.text}
-                  //       </span>
-                  //     </div>
-                  //   </td>
-                  // </tr>
                   <TableRow place={place} key={place.id} />
                 ))}
               </tbody>
