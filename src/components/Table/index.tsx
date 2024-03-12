0ffe3d6f4d9bf8902ccf30b9b13b6536326a62f1
Spenser Schwartz <spensernet@gmail.com>
2024-03-12 14:33:44 -0500

    TableRow init

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,6 +5,7 @@ import { LoginModal } from "~/components/modal";
 import { HeartIcon } from "~/icons";
 import type { PlaceNew } from "~/types/google";
 import { useAddLikeFromUser } from "~/utils/hooks";
+import TableRow from "./components";
 
 interface TableProps {
   cityId: string | null;
@@ -51,42 +52,43 @@ export default function Table({ cityId, places }: TableProps) {
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
                 {places.map((place) => (
-                  <tr key={place.id}>
-                    {/* Adjusted cells for ellipsis */}
-                    <td
-                      className="w-1/3 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
-                      style={{
-                        minWidth: "100px",
-                        maxWidth: "200px",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {place.displayName?.text}
-                    </td>
+                  // <tr key={place.id}>
+                  //   {/* Adjusted cells for ellipsis */}
+                  //   <td
+                  //     className="w-1/3 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
+                  //     style={{
+                  //       minWidth: "100px",
+                  //       maxWidth: "200px",
+                  //       overflow: "hidden",
+                  //       textOverflow: "ellipsis",
+                  //       whiteSpace: "nowrap",
+                  //     }}
+                  //   >
+                  //     {place.displayName?.text}
+                  //   </td>
 
-                    <td
-                      className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell"
-                      style={{
-                        minWidth: "100px",
-                        maxWidth: "300px",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {place.editorialSummary?.text}
-                    </td>
-                    <td className="relative py-4 text-right text-sm font-medium ">
-                      <div className="flex items-center justify-center">
-                        <HeartIcon enabled={false} />
-                        <span className="sr-only">
-                          Like Button for {place.displayName?.text}
-                        </span>
-                      </div>
-                    </td>
-                  </tr>
+                  //   <td
+                  //     className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell"
+                  //     style={{
+                  //       minWidth: "100px",
+                  //       maxWidth: "300px",
+                  //       overflow: "hidden",
+                  //       textOverflow: "ellipsis",
+                  //       whiteSpace: "nowrap",
+                  //     }}
+                  //   >
+                  //     {place.editorialSummary?.text}
+                  //   </td>
+                  //   <td className="relative py-4 text-right text-sm font-medium ">
+                  //     <div className="flex items-center justify-center">
+                  //       <HeartIcon enabled={false} />
+                  //       <span className="sr-only">
+                  //         Like Button for {place.displayName?.text}
+                  //       </span>
+                  //     </div>
+                  //   </td>
+                  // </tr>
+                  <TableRow place={place} key={place.id} />
                 ))}
               </tbody>
             </table>
