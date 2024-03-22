a33e71b74963f25bdcab3891804b7ed752d8baf5
Spenser Schwartz <spensernet@gmail.com>
2024-03-22 12:14:43 -0500

    utils

---
+++ src/app/_components/utils.ts
@@ -0,0 +1,91 @@
+export const quickLaunchCities = [
+  "London",
+  "Berlin",
+  "New York City",
+  "Paris",
+  "Munich",
+  "Amsterdam",
+  "Rome",
+  "Barcelona",
+  "Madrid",
+  "Prague",
+  "Vienna",
+  "Budapest",
+  "Dublin",
+  "Lisbon",
+  "Brussels",
+  "Copenhagen",
+  "Stockholm",
+  "Athens",
+  "Helsinki",
+  "Oslo",
+  "Warsaw",
+  "Zurich",
+  "Moscow",
+  "Istanbul",
+  "Saint Petersburg",
+  "Bucharest",
+  "Edinburgh",
+  "Venice",
+  "Florence",
+  "Milan",
+  "Naples",
+  "Luxembourg City",
+  "Reykjavik",
+  "Monaco",
+  "Vatican City",
+];
+
+export const travelQuotes = [
+  {
+    author: "Susan Sontag",
+    quote: `I haven't been everywhere, but it's on my list.`,
+  },
+  {
+    author: "Mark Twain",
+    quote: `Twenty years from now you will be more disappointed by the things you didn’t do than by the ones you did do.`,
+  },
+  {
+    author: "Anonymous",
+    quote: "We travel not to escape life, but for life not to escape us.",
+  },
+  {
+    author: "André Gide",
+    quote:
+      "Man cannot discover new oceans unless he has the courage to lose sight of the shore.",
+  },
+  {
+    author: "Oliver Wendell Holmes Jr.",
+    quote:
+      "A mind that is stretched by a new experience can never go back to its old dimensions.",
+  },
+  {
+    author: "Helen Keller",
+    quote: "Life is either a daring adventure or nothing at all.",
+  },
+  {
+    author: `Roy M. Goodman`,
+    quote: `Remember that happiness is a way of travel – not a destination.`,
+  },
+  {
+    author: `Neale Donald Walsch`,
+    quote: `Life begins at the end of your comfort zone.`,
+  },
+  {
+    author: `Saint Augustine`,
+    quote: `The world is a book and those who do not travel read only one page.`,
+  },
+];
+
+export const unknownClerkCity = {
+  id: "custom",
+  name: "Custom City",
+};
+
+export const unknownClerkUser = {
+  id: "user_2YxVp66u0zvMgoN7zOSm6sJiiHb",
+};
+
+export const unknownItinerary = {
+  id: "fake",
+};
