d6ab2cd2a528dd7a5d84672e0d5ae6fb736e421f
Spenser Schwartz <spensernet@gmail.com>
2024-03-22 12:56:51 -0500

    Home Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import "~/styles/globals.css";
-
-import { Inter } from "next/font/google";
-
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "~/styles/globals.css";
 import { TRPCReactProvider } from "~/trpc/react";
+
+import { Footer, Header } from "@/components/layout";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,7 +25,22 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`font-sans ${inter.variable}`}>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
+          {/* <TRPCReactProvider>{children}</TRPCReactProvider> */}
+          <div className="bg-white tracking-tight text-gray-900 antialiased">
+            <Header />
+            <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
+              {/* Spacing between header and main content */}
+              <div className="mb-24 md:mb-20" />
+
+              {/* Main Content */}
+              <TRPCReactProvider>{children}</TRPCReactProvider>
+
+              {/* Grow the page so that footer is at bottom of page if there is no scroll */}
+              <div className="flex-grow"></div>
+            </div>
+
+            <Footer />
+          </div>
         </body>
       </html>
     </ClerkProvider>
