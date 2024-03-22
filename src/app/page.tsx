122005940a95228ed842e71cccd190eac4dcf3e0
Spenser Schwartz <spensernet@gmail.com>
2024-03-22 11:27:16 -0500

    Clerk middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { api } from "~/trpc/server";
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
   const whatIsHere = await api.city.getAll();
-  console.log("whatIsHere", whatIsHere);
+  // console.log("whatIsHere", whatIsHere);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
