09b7225e8edee49f1a1b155feb95fb194907c951
Spenser Schwartz <spensernet@gmail.com>
2024-03-22 12:57:15 -0500

    Footer

--- src/app/_components/layout/index.tsx
+++ src/app/_components/layout/index.tsx
@@ -1,6 +1,7 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import HeaderLogo from "./HeaderLogo";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 
-export { Header, HeaderLogo, Logo, MobileMenu };
+export { Footer, Header, HeaderLogo, Logo, MobileMenu };
