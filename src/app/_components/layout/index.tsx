1b659df640496522024f41c2bb6454b92ff0dbbf
Spenser Schwartz <spensernet@gmail.com>
2024-03-22 12:15:59 -0500

    Barrel import layout components

---
+++ src/app/_components/layout/index.tsx
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import HeaderLogo from "./HeaderLogo";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+
+export { Header, HeaderLogo, Logo, MobileMenu };
