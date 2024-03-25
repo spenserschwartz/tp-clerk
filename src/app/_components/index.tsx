72e213834a1e68dd37e3bfab9583589605e089f6
Spenser Schwartz <spensernet@gmail.com>
2024-03-25 16:23:02 -0700

    Select

--- src/app/_components/index.tsx
+++ src/app/_components/index.tsx
@@ -1,6 +1,15 @@
 import Button from "./Button";
+import PlacesAutoComplete from "./google/PlacesAutoComplete";
 import { LoadingPage } from "./loading/LoadingPage";
 import LoadingSection from "./loading/LoadingSection";
 import LoadingSpinner from "./loading/LoadingSpinner";
+import Select from "./Select";
 
-export { Button, LoadingPage, LoadingSection, LoadingSpinner };
+export {
+  Button,
+  LoadingPage,
+  LoadingSection,
+  LoadingSpinner,
+  PlacesAutoComplete,
+  Select,
+};
