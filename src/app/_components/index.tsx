5ebaa6c87523f69c49c215fd93c75d031f58bfae
Spenser Schwartz <spensernet@gmail.com>
2024-03-25 15:49:25 -0700

    barrel import

---
+++ src/app/_components/index.tsx
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import { LoadingPage } from "./loading/LoadingPage";
+import LoadingSection from "./loading/LoadingSection";
+import LoadingSpinner from "./loading/LoadingSpinner";
+
+export { Button, LoadingPage, LoadingSection, LoadingSpinner };
