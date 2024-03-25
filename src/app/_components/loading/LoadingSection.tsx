092964e8f76c43fda2d0a9fed23cc0fb208622ab
Spenser Schwartz <spensernet@gmail.com>
2024-03-25 14:56:30 -0700

    LoadingSection

---
+++ src/app/_components/loading/LoadingSection.tsx
@@ -0,0 +1,16 @@
+import { LoadingSpinner } from "..";
+
+const LoadingSection = () => {
+  return (
+    <div className="flex min-h-48 w-full max-w-xl flex-col items-center rounded-xl border border-gray-600 bg-gray-300 p-6">
+      <LoadingSpinner size={100} />
+      <p className="mb-8 text-gray-600">
+        Our AI robots are hard at work making your itinerary!
+      </p>
+
+      <p>{"Live life with no excuses, travel with no regret. -Oscar Wilde "}</p>
+    </div>
+  );
+};
+
+export default LoadingSection;
