a21160e8e627217a3ed7bc7506393558a218d84a
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:01:07 -0700

    TextBoldIcon

---
+++ src/app/_components/icons/TextBold.tsx
@@ -0,0 +1,21 @@
+const TextBoldIcon = () => {
+  return (
+    <svg
+      className="h-4 w-4 flex-shrink-0"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14 12a4 4 0 0 0 0-8H6v8" />
+      <path d="M15 20a4 4 0 0 0 0-8H6v8Z" />
+    </svg>
+  );
+};
+
+export default TextBoldIcon;
