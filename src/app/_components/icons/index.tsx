727949914f453fcab7822030266b1280d239bbc1
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:06:18 -0700

    TextUnderLineIcon

--- src/app/_components/icons/index.tsx
+++ src/app/_components/icons/index.tsx
@@ -1,5 +1,9 @@
 import AvatarPlaceholder from "./AvatarPlaceholder";
 import GitHubIcon from "./Github";
 import LinkedInIcon from "./LinkedIn";
+import TextBoldIcon from "./TextBold";
+import TextBulletListIcon from "./TextBullet";
+import TextItalicIcon from "./TextItalic";
+import TextLinkIcon from "./TextLink";
 
 export { AvatarPlaceholder, GitHubIcon, LinkedInIcon };
