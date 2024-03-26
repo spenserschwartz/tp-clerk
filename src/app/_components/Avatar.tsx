87854eecbb474d0f90aee9184d7df38c529d00f7
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 07:32:28 -0700

    next/router to next/navigation

--- src/app/_components/Avatar.tsx
+++ src/app/_components/Avatar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
 
 import { AvatarPlaceholder } from "./icons";
