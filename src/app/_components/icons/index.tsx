4cd95109e20aa1284ac377aa41304ca4342a7d9b
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:06:57 -0700

    barrel import

--- src/app/_components/icons/index.tsx
+++ src/app/_components/icons/index.tsx
@@ -5,5 +5,17 @@ import TextBoldIcon from "./TextBold";
 import TextBulletListIcon from "./TextBullet";
 import TextItalicIcon from "./TextItalic";
 import TextLinkIcon from "./TextLink";
+import TextOrderedListIcon from "./TextOrderedList";
+import TextUnderlineIcon from "./TextUnderline";
 
-export { AvatarPlaceholder, GitHubIcon, LinkedInIcon };
+export {
+  AvatarPlaceholder,
+  GitHubIcon,
+  LinkedInIcon,
+  TextBoldIcon,
+  TextBulletListIcon,
+  TextItalicIcon,
+  TextLinkIcon,
+  TextOrderedListIcon,
+  TextUnderlineIcon,
+};
