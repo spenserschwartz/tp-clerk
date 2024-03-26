94715dc73a4cd9a27f44628cd77302f1139fabea
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:49:34 -0700

    useEditItineraryTitle

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,11 +1,13 @@
 import useAIGenerateItinerary from "./useAIGenerateItinerary";
 import useCreateItinerary from "./useCreateItinerary";
+import useEditItineraryTitle from "./useEditItineraryTitle";
 import useEditItineraryUserNotes from "./useEditItineraryUserNotes";
 import useIsSmallScreen from "./useIsSmallScreen";
 
 export {
   useAIGenerateItinerary,
   useCreateItinerary,
+  useEditItineraryTitle,
   useEditItineraryUserNotes,
   useIsSmallScreen,
 };
