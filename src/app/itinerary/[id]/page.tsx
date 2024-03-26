992f756166ddaa3026078826b81229189cadc90f
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:55:21 -0700

    in progress

--- src/app/itinerary/[id]/page.tsx
+++ src/app/itinerary/[id]/page.tsx
@@ -2,7 +2,7 @@ import { SignedIn, useUser } from "@clerk/nextjs";
 import { type GetStaticProps } from "next";
 import { useState, type ReactElement } from "react";
 
-import { Itinerary } from "@/components";
+import { Itinerary, ItineraryNotes, ItineraryTitle } from "@/components";
 
 const ItineraryPage = () => {
   return <div>ItineraryPage</div>;
