d921ac1f3550337ed0bb2d0db04bbadb6b0baac7
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 16:34:38 -0700

    update

--- src/app/itinerary/[id]/page.tsx
+++ src/app/itinerary/[id]/page.tsx
@@ -5,8 +5,12 @@ import { useState, type ReactElement } from "react";
 import { Itinerary, ItineraryNotes, ItineraryTitle } from "@/components";
 import { DeleteItineraryModal } from "@/modals";
 
-const ItineraryPage = () => {
-  return <div>ItineraryPage</div>;
-};
+// const ItineraryPage = ({params: {itineraryID}}) => {
+//   return <div>ItineraryPage</div>;
+// };
 
-export default ItineraryPage;
+// export default ItineraryPage;
+
+export default function ItineraryPage({ params }: { params: { id: string } }) {
+  return <div>{`Id: ${params.id}`}</div>;
+}
