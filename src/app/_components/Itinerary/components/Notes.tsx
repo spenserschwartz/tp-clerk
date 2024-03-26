ee61ff76c9734345fcf8b0717cd0d275355203de
Spenser Schwartz <spensernet@gmail.com>
2024-03-26 14:34:34 -0700

    ItineraryNotes

---
+++ src/app/_components/Itinerary/components/Notes.tsx
@@ -0,0 +1,16 @@
+import { TextEditor } from "@/components/tiptap";
+import type { ItineraryWithCityInfoType } from "~/types/router";
+
+interface ItineraryNotesProps {
+  data: ItineraryWithCityInfoType;
+}
+
+const ItineraryNotes = ({ data }: ItineraryNotesProps) => {
+  return (
+    <div className="mt-10">
+      <TextEditor data={data} />
+    </div>
+  );
+};
+
+export default ItineraryNotes;
