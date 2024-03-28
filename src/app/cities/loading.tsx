56694246e2b289775be7080794c362be14b18472
Spenser Schwartz <spensernet@gmail.com>
2024-03-28 16:19:37 -0700

    Cities loading page

---
+++ src/app/cities/loading.tsx
@@ -0,0 +1,7 @@
+import { LoadingPage } from "@/components";
+
+const CitiesLoadingPage = () => {
+  return <LoadingPage />;
+};
+
+export default CitiesLoadingPage;
---
+++ src/app/cities/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+// import { LoadingPage } from "~/components";
+import { api } from "~/trpc/server";
+
+const CitiesPage = async () => {
+  //   const { data: cityNames, isLoading } = api.city.getAllCityNames.useQuery();
+  const cityNames = await api.city.getAllCityNames();
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <p className="text-center">
+        These are the current cities that TravelPerfect is working with:
+      </p>
+      <p className="text-orange-400">(Click to navigate to city)</p>
+      <ul className="flex flex-col  text-center">
+        {cityNames?.map((cityName) => (
+          <Link
+            className="mt-2"
+            key={`CitiesList:${cityName}`}
+            href={`/city/${cityName}`}
+          >
+            {cityName}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CitiesPage;
