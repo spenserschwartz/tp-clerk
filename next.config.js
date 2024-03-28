da39ac06799899ac845e9f1fe1d95bd8d9c5e8b6
Spenser Schwartz <spensernet@gmail.com>
2024-03-28 08:18:13 -0700

    match next.config.mjs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,51 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { hostname: "images.clerk.dev" },
+      { hostname: "img.clerk.com" },
+      { hostname: "images.unsplash.com" },
+      { hostname: "tailwindui.com" },
+      { hostname: "travelperfect-bucket.s3.us-west-1.amazonaws.com" },
+      { hostname: "www.gravatar.com" },
+      { hostname: "maps.googleapis.com" },
+      { hostname: "maps.google.com" },
+      { hostname: "places.googleapis.com" }, // Places API (New)
+      { hostname: "travelperfect.io" },
+    ],
+  },
 
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async redirects() {
+    return [
+      {
+        source: "/user",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+
+  /**
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+};
 export default config;
