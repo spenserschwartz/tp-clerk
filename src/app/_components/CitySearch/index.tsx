e71b173b10692d901d524ab78658ce0b2f1683bb
Spenser Schwartz <spensernet@gmail.com>
2024-03-28 16:41:34 -0700

    CitySearch in progress

---
+++ src/app/_components/CitySearch/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { APIProvider as GoogleAPIProvider } from "@vis.gl/react-google-maps";
+import { useRouter } from "next/navigation";
+
+import { LoadingPage, PlacesAutoComplete } from "@/components";
+import { convertFormattedAddressToUrlPath } from "~/lib/utils";
+import type { AutocompleteRequest, PlaceResult } from "~/types/google";
+
+const CitySearch = () => {
+  const router = useRouter();
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) return <LoadingPage />;
+
+  const autocompleteRequest: AutocompleteRequest = {
+    input: "",
+    types: ["(cities)"],
+  };
+
+  // Route to the dynamic city page
+  const handleSelectCity = (city: PlaceResult | null) => {
+    // Make a dynamic route to the city's page
+    const { formatted_address } = city ?? {};
+    if (!formatted_address) return;
+
+    const dynamicRoute = convertFormattedAddressToUrlPath(formatted_address);
+    // void router.push("/things-to-do" + dynamicRoute);
+    // void router.push({
+    //   pathname: "/things-to-do" + dynamicRoute,
+    // });
+    void router.push("/things-to-do" + dynamicRoute);
+  };
+
+  return (
+    <div>
+      <GoogleAPIProvider apiKey={apiKey}>
+        <PlacesAutoComplete
+          requestOptions={autocompleteRequest}
+          setSelected={handleSelectCity}
+        />
+      </GoogleAPIProvider>
+    </div>
+  );
+};
+
+export default CitySearch;
---
+++ src/app/city-search/page.tsx
@@ -0,0 +1,11 @@
+// import { APIProvider as GoogleAPIProvider } from "@vis.gl/react-google-maps";
+
+import { CitySearch, LoadingPage } from "@/components";
+
+const CitySearchPage = () => {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) return <LoadingPage />;
+  return <CitySearch />;
+};
+
+export default CitySearchPage;
