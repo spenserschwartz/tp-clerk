220739703b7d19d8c7a0aa8fff200c41ae3b6f03
Spenser Schwartz <spensernet@gmail.com>
2024-03-28 12:48:17 -0700

    QuickLaunchItineraryType

---
+++ src/types/common.ts
@@ -0,0 +1,8 @@
+import type { ItineraryWithCityInfoType } from "./router";
+
+// QuickLaunchItineraryType represents the data structure that is passed from the QuickLaunchTool component
+// It is NOT ItineraryWithCityInfoType because it is not retrieved from the database, so we only have the
+// "details" key and set everything else to an optional property (Partial) that will never be there
+type JustDetailsItineraryType = Pick<ItineraryWithCityInfoType, "details">;
+export type QuickLaunchItineraryType = JustDetailsItineraryType &
+  Partial<Omit<ItineraryWithCityInfoType, "details">>;
