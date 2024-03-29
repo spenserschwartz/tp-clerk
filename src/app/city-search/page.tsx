ed75a03378b05614b35848a815af66c7ff5f114d
Spenser Schwartz <spensernet@gmail.com>
2024-03-29 12:38:42 -0700

    CitySearchPage

--- src/app/city-search/page.tsx
+++ src/app/city-search/page.tsx
@@ -1,8 +1,6 @@
-import { CitySearch, LoadingPage } from "@/components";
+import { CitySearch } from "@/components";
 
 const CitySearchPage = () => {
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-  if (!apiKey) return <LoadingPage />;
   return <CitySearch />;
 };
 
