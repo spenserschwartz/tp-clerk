3e9e33a2b379a120fea15f6d77a451367d27c88c
Spenser Schwartz <spensernet@gmail.com>
2024-04-01 14:22:51 -0700

    Update minimum width

--- src/app/_components/modal/Wrapper.tsx
+++ src/app/_components/modal/Wrapper.tsx
@@ -38,7 +38,7 @@ const ModalWrapper = ({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 {/* Modal Content */}
                 {children}
               </Dialog.Panel>
