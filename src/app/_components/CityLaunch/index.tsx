e98dd55273d3ab7ec461776de818e6345bcbf92b
Spenser Schwartz <spensernet@gmail.com>
2024-04-01 22:49:26 -0700

    CityLaunch

--- src/app/_components/CityLaunch/index.tsx
+++ src/app/_components/CityLaunch/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import { addDays, format as formatDate } from "date-fns";
 import React, { useEffect, useState } from "react";
