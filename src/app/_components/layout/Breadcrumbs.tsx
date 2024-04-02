6e0b4cefb2291a5d9978698b529646c8cc44378d
Spenser Schwartz <spensernet@gmail.com>
2024-04-02 13:21:20 -0700

    Breadcrumbs

---
+++ src/app/_components/layout/Breadcrumbs.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const getDisplayName = (segment: string) => {
+  return segment
+    .replace(/#/g, "") // Remove hashtags
+    .split("-") // Split the string by hyphens
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+    .join(" "); // Join the words with spaces
+};
+
+export default function Breadcrumbs() {
+  const { user } = useUser();
+  const pathname = usePathname();
+
+  // Split the URL path into segments and filter out empty strings
+  const pathSegments = pathname.split("/").filter(Boolean);
+
+  const pathsToNotShow: Record<string, boolean> = { city: true };
+
+  // Create breadcrumb pages from URL segments
+  const pages = pathSegments
+    .filter((path) => !pathsToNotShow[path]) // Filter out paths that we don't want to show
+    .map((segment, index) => {
+      const href = "/" + pathSegments.slice(0, index + 1).join("/");
+      const isCurrentPage = index === pathSegments.length - 1;
+
+      // Conditional logic to handle user
+      if (pathSegments[index - 1] === "user") {
+        return {
+          name: user?.fullName ?? "userName",
+          href,
+          current: isCurrentPage,
+        };
+      }
+
+      return { name: getDisplayName(segment), href, current: isCurrentPage };
+    });
+
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol role="list" className="mb-2 flex items-center space-x-4">
+        {/* Home Icon */}
+        <li key="home-icon">
+          <Link href="/" className="text-gray-400 hover:text-gray-500">
+            <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <span className="sr-only">Home</span>
+          </Link>
+        </li>
+
+        {/* Rest of breadcrumbs */}
+        {pages.map((page) => (
+          <li key={page.name}>
+            <div className="flex items-center">
+              <ChevronRightIcon
+                className="h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+
+              <Link
+                href={page.href}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? "page" : undefined}
+              >
+                {page.name}
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
--- src/app/_components/layout/index.tsx
+++ src/app/_components/layout/index.tsx
@@ -1,7 +1,8 @@
+import Breadcrumbs from "./Breadcrumbs";
 import Footer from "./Footer";
 import Header from "./Header";
 import HeaderLogo from "./HeaderLogo";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 
-export { Footer, Header, HeaderLogo, Logo, MobileMenu };
+export { Breadcrumbs, Footer, Header, HeaderLogo, Logo, MobileMenu };
