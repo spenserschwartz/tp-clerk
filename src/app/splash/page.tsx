a7cb9e1af40a8e440658cc965c270f1241e0e596
Spenser Schwartz <spensernet@gmail.com>
2024-04-04 22:30:48 -0700

    Splash

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import { api } from "~/trpc/server";
 
 const SplashPage = async () => {
-  // const whatIsHere = await api.city.getAll();
-  // const str = JSON.stringify(whatIsHere, null, 2);
-
-  const allItineraries = await api.itinerary.getAll();
-  const str = JSON.stringify(allItineraries, null, 2);
-
-  return <div>{str}</div>;
+  return <div>Splash</div>;
 };
 
 export default SplashPage;
