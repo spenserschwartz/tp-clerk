cb641e8b8c2b1e2bda3576b25eeee09bde8ddbeb
Spenser Schwartz <spensernet@gmail.com>
2024-04-10 15:31:05 -0700

    Table

--- src/app/_components/Table/index.tsx
+++ src/app/_components/Table/index.tsx
@@ -1,31 +1,33 @@
+"use client";
 import { useUser } from "@clerk/nextjs";
-import { useMemo, useState } from "react";
-import { api } from "~/trpc/react";
+import { useState } from "react";
 
 import { TableRow } from "@/components";
 import { LoginModal } from "@/modals";
 import type { PlaceNew } from "~/types/google";
+import type { GetAllLikesByUserInCityType } from "~/types/router";
 
 interface TableProps {
+  allLikesByUserInCity: GetAllLikesByUserInCityType[];
   cityId: string;
   places: PlaceNew[] | undefined;
 }
 
-export default function Table({ cityId, places }: TableProps) {
+export default function Table({
+  allLikesByUserInCity,
+  cityId,
+  places,
+}: TableProps) {
   const { isSignedIn } = useUser();
   const [openModal, setOpenModal] = useState(false);
-  const { data: allLikesByUserInCity } = api.likes.getAllByUserInCity.useQuery({
-    cityId: cityId ?? "",
-  });
 
-  // Create a set of all the places the user has liked, useMemo to avoid re-creating the set on every render
-  const likedPlacesSet = useMemo(() => {
-    return new Set(allLikesByUserInCity?.map((like) => like.placeId));
-  }, [allLikesByUserInCity]);
+  const allLikesByUserInCitySet = new Set(
+    allLikesByUserInCity?.map((like) => like.placeId),
+  );
 
   if (!places) return null;
   return (
-    <div className="mt-8 flow-root w-full max-w-6xl">
+    <div className="mb-2 mt-8 flow-root w-full max-w-6xl">
       <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
@@ -58,7 +60,8 @@ export default function Table({ cityId, places }: TableProps) {
                     isSignedIn={isSignedIn ?? false}
                     place={place}
                     setOpenModal={setOpenModal}
-                    userHasLikedPlace={likedPlacesSet.has(place.id)}
+                    // userHasLikedPlace={set.has("ChIJLZlhwACdQIYRFvnVL8mq8BM")}
+                    userHasLikedPlace={allLikesByUserInCitySet.has(place.id)}
                   />
                 ))}
               </tbody>
