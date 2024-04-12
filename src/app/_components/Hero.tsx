61cfb7ce5cc8f8a1e14f902c9b57e961f976a120
Spenser Schwartz <spensernet@gmail.com>
2024-04-12 11:23:15 -0700

    FadeUpWrapper on Carousel

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,13 +1,6 @@
 "use client";
 import { Carousel } from "@/components";
-import { ZoomInUpWrapper } from "@/framer-motion";
-
-const slides: string[] = [
-  "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80",
-  "https://images.unsplash.com/photo-1529180184525-78f99adb8e98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-  "https://images.unsplash.com/photo-1560969185-ee68ef9080c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-  "https://images.unsplash.com/photo-1578666062144-080ac96e3e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-];
+import { FadeUpWrapper, ZoomInUpWrapper } from "@/framer-motion";
 
 export default function Hero() {
   return (
@@ -89,8 +82,9 @@ export default function Hero() {
           </ZoomInUpWrapper>
 
           {/* Image Carousel */}
-          {/* <div className="max-w-2xl"><Carousel slides={slides} /></div> */}
-          <Carousel />
+          <FadeUpWrapper>
+            <Carousel />
+          </FadeUpWrapper>
         </div>
       </div>
     </section>
