bd4a1589584db011e1a591f7fd33fde028713240
Spenser Schwartz <spensernet@gmail.com>
2024-04-16 13:58:22 -0700

    convert to client component pending TRPC revalidation for server components

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,23 +1,32 @@
-import { api } from "~/trpc/server";
+"use client";
+// import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 
-import { ItineraryImageGrid } from "@/components";
+import { ItineraryImageGrid, LoadingPage } from "@/components";
 import type { ItineraryWithCityInfoType } from "~/types/router";
 
 interface UserPageProps {
   params: { id: string };
 }
 
-const UserPage = async ({ params }: UserPageProps) => {
+const UserPage = ({ params }: UserPageProps) => {
   const { id: userId } = params;
 
-  const userData = await api.profile.getUserById({ userId });
-  const itinerariesByUser = await api.itinerary.getByUserId({ userId });
+  const { data: userData, isLoading: userDataIsLoading } =
+    api.profile.getUserById.useQuery({ userId });
+  const { data: itinerariesByUser, isLoading: itinerariesDataIsLoading } =
+    api.itinerary.getByUserId.useQuery({ userId });
+
+  // const userData = await api.profile.getUserById({ userId });
+  // const itinerariesByUser = await api.itinerary.getByUserId({ userId });
 
   const pageTitle =
     userData?.firstName && userData?.lastName
       ? `${userData.firstName} ${userData.lastName}`
       : userId;
 
+  if (userDataIsLoading || itinerariesDataIsLoading) return <LoadingPage />;
+  if (!userData) return <div>User not found</div>;
   return (
     <main className="flex w-full flex-col items-center px-2">
       <div className="relative flex w-full items-center justify-center">
