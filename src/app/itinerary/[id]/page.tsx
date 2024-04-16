d8c3e5ee65769e774643e08b9327e2ea74cd8f0b
Spenser Schwartz <spensernet@gmail.com>
2024-04-16 13:58:12 -0700

    convert to client component pending TRPC revalidation for server components

--- src/app/itinerary/[id]/page.tsx
+++ src/app/itinerary/[id]/page.tsx
@@ -1,18 +1,20 @@
+"use client";
 import {
   DeleteItinerary,
   Itinerary,
   ItineraryNotes,
   ItineraryTitle,
+  LoadingPage,
 } from "@/components";
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 
-export default async function ItineraryPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const data = await api.itinerary.getByID({ id: params.id });
+export default function ItineraryPage({ params }: { params: { id: string } }) {
+  // const data = await api.itinerary.getByID({ id: params.id });
+  const { data, isLoading } = api.itinerary.getByID.useQuery({ id: params.id });
 
+  if (isLoading) return <LoadingPage />;
+  if (!data) return <div>Itinerary not found</div>;
   return (
     <div className="flex flex-col items-center">
       <ItineraryTitle data={data} />
