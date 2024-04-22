9f258f7cfd8a080c144b09309f49d749f87d8238
Spenser Schwartz <spensernet@gmail.com>
2024-04-22 10:59:46 -0700

    splash

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
-const SplashPage = () => {
-  return <div>Splash</div>;
+// const ToDoComponent = ({ todo, modalLogic }) => {
+//   // modal logic is triggered and passed up to parent
+//   return (
+//     <div>
+//       {todo}
+//     </div>
+//   );
+// };
+
+const Page = () => {
+  // const todos = [todo1, todo2, todo3, ...];
+  // MODAL LOGIC HERE
+
+  return (
+    <div>
+      {/* {todos.map((todo) => (
+      <ToDoComponent todo={todo} modalLogic={modalLogic} />
+    )}
+
+    <ModalThatDoesSomething /> */}
+    </div>
+  );
 };
 
-export default SplashPage;
+export default Page;
 
 // Tower of London GoogleId
 // ChIJ3TgfM0kDdkgRZ2TV4d1Jv6g
