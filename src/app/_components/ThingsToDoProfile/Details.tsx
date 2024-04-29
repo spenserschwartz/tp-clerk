8a094f9f4cf824eb9ce195f53a8ad8097b7b555f
Spenser Schwartz <spensernet@gmail.com>
2024-04-29 16:18:38 -0700

    googleCityName

--- src/app/_components/ThingsToDoProfile/Details.tsx
+++ src/app/_components/ThingsToDoProfile/Details.tsx
@@ -9,13 +9,12 @@ import type { GetAllLikesByUserInCityType } from "~/types/router";
 
 interface ThingsToDoDetailsProps {
   allLikesByUserInCity: GetAllLikesByUserInCityType[];
-  googlePlaceId: string;
-  //   placeResult: PlaceResultWithLatLng;
+  googleCityName: string;
 }
 
 const ThingsToDoDetails = ({
   allLikesByUserInCity,
-  googlePlaceId,
+  googleCityName,
 }: ThingsToDoDetailsProps) => {
   const placesLib = useMapsLibrary("places");
   const [placeResult, setPlaceResult] = useState<PlaceResult | undefined>(
@@ -36,34 +35,24 @@ const ThingsToDoDetails = ({
 
   // Fetch details from Google Places API
   useEffect(() => {
-    if (!placesService || !googlePlaceId) return;
+    if (!placesService || !googleCityName) return;
 
-    placesService.textSearch(
-      { query: "Abbotsford BC Canda" },
-      (result, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && result) {
-          console.log("textSearch result", result);
-          setPlaceResult(result[0]);
-          const photos = result[0]?.photos?.map((photo) => {
-            return photo.getUrl();
-          });
-          setImages(photos?.slice(0, 5) ?? []);
-        } else {
-          console.log("Failed to fetch place details:", status);
-        }
-      },
-    );
-  }, [placesService, googlePlaceId]);
-
-  console.log("googlePlaceId", googlePlaceId);
-  console.log("placeResult", placeResult);
-  console.log("images", images);
-  console.log("placeResult lat", placeResult?.geometry?.location?.lat());
+    placesService.textSearch({ query: googleCityName }, (result, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && result) {
+        console.log("textSearch result", result);
+        setPlaceResult(result[0]);
+        const photos = result[0]?.photos?.map((photo) => {
+          return photo.getUrl();
+        });
+        setImages(photos?.slice(0, 5) ?? []);
+      } else {
+        console.log("Failed to fetch place details:", status);
+      }
+    });
+  }, [placesService, googleCityName]);
 
   return (
     <div className="mt-4 flex w-full justify-between border-2 border-red-400 ">
-      {/* <ImageGallery images={images} /> */}
-
       <Image
         src={images[0] ?? "/images/placeholder.png"}
         alt="Alt is required by Next Image"
