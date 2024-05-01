1eab6fe7f259737dbcb82f2bdc5b88ec7868426d
Spenser Schwartz <spensernet@gmail.com>
2024-04-30 23:01:27 -0700

    getUserByUsername to be checked, getUserById to be refactored

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -5,37 +5,35 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-// ! This file needs to be cleaned up and refactored
 export const profileRouter = createTRPCRouter({
-  // getUserByUsername: publicProcedure
-  //   .input(z.object({ username: z.string() }))
-  //   .query(async ({ input }) => {
-  //     const [user] = await clerkClient.users.getUserList({
-  //       username: [input.username],
-  //     });
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const { data } = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
 
-  //     if (!user) {
-  //       // if we hit here we need a unsantized username so hit api once more and find the user.
-  //       const users = await clerkClient.users.getUserList({
-  //         limit: 200,
-  //       });
-  //       const user = users.find((user) =>
-  //         user.externalAccounts.find(
-  //           (account) => account.username === input.username,
-  //         ),
-  //       );
-  //       if (!user) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "User not found",
-  //         });
-  //       }
-  //       return filterUserForClient(user);
-  //     }
+      if (!data?.[0]) {
+        // if we hit here we need a unsantized username so hit api once more and find the user.
+        const { data: userList } = await clerkClient.users.getUserList({
+          limit: 200,
+        });
+        const user = userList.find((user) =>
+          user.externalAccounts.find(
+            (account) => account.username === input.username,
+          ),
+        );
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user);
+      } else return filterUserForClient(data[0]);
+    }),
 
-  //     return filterUserForClient(user);
-  //   }),
-
+  // ! This file needs to be cleaned up and refactored
   getUserById: publicProcedure
     .input(z.object({ userId: z.string() }))
     .query(async ({ input }) => {
