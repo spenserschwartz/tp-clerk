af5e823423327c5a358f22e56a11e8fe338aa2ee
Spenser Schwartz <spensernet@gmail.com>
2024-05-03 12:26:31 -0700

    update

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => (
-  <div className="flex h-full items-center justify-center">
+  <div className="flex h-full items-center justify-center ">
     <SignUp />
   </div>
 );
